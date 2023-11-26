acb1cca2a9967e2c6bbecfface5b94d11f1c6c8b
uncommon-type <uncommon.type@rambler.ru>
2023-11-26 11:42:48 +0300

    [#19] Render <Input/> component within <NewJob/>

--- src/screens/addJob/components/NewJob.jsx
+++ src/screens/addJob/components/NewJob.jsx
@@ -1,9 +1,12 @@
-import { SaveButton } from '@screens/common/Buttons/SaveButton';
+import { Input } from '@screens/common/Inputs/Input';
 import { TextAreaInput } from '@screens/common/Inputs/TextAreaInput';
+import { SaveButton } from '@screens/common/Buttons/SaveButton';
 
 export const NewJob = () => {
   return (
     <form className="add-job-form flow flow-space-xl">
+      <Input showLabel={true} label="Role" name="role" />
+      <Input showLabel={true} label="Company" name="company" />
       <TextAreaInput
         edit={true}
         showLabel={true}
