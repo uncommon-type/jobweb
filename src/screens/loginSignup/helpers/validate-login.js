d313f9e42759fbca60c45ec55be889078102e01c
uncommon-type <uncommon.type@rambler.ru>
2023-11-29 12:41:23 +0300

    [#25] Update error messages in validateLogin, validateSignup

--- src/screens/loginSignup/helpers/validate-login.js
+++ src/screens/loginSignup/helpers/validate-login.js
@@ -2,11 +2,17 @@ export const validateLogin = (data) => {
   let errors = [];
 
   if (!data.username) {
-    errors = [...errors, { name: 'email', message: 'Email is required' }];
+    errors = [
+      ...errors,
+      { name: 'email', message: "That doesn't look like a valid email" },
+    ];
   }
 
   if (!data.password) {
-    errors = [...errors, { name: 'password', message: 'Password is required' }];
+    errors = [
+      ...errors,
+      { name: 'password', message: "That doesn't look like a valid password" },
+    ];
   }
 
   if (errors.length > 0) {
--- src/screens/loginSignup/helpers/validate-signup.js
+++ src/screens/loginSignup/helpers/validate-signup.js
@@ -2,15 +2,24 @@ export const validateSignup = (data) => {
   let errors = [];
 
   if (!data.name) {
-    errors = [...errors, { name: 'name', message: 'Name is required' }];
+    errors = [
+      ...errors,
+      { name: 'name', message: 'You need to give your name' },
+    ];
   }
 
   if (!data.email) {
-    errors = [...errors, { name: 'email', message: 'Email is required' }];
+    errors = [
+      ...errors,
+      { name: 'email', message: "That doesn't look like a valid email" },
+    ];
   }
 
   if (!data.password) {
-    errors = [...errors, { name: 'password', message: 'Password is required' }];
+    errors = [
+      ...errors,
+      { name: 'password', message: "That doesn't look like a valid password" },
+    ];
   }
 
   if (errors.length > 0) {
