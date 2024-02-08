7c686cbe3ae65ce358e9f726023154068b8b877e
uncommon-type <uncommon.type@rambler.ru>
2024-02-08 10:15:46 +0300

    [#36] Pass jobId prop to CheckboxGroup directly instead of extracting it from URL parameters with useParams hook

--- src/screens/common/Inputs/CheckboxGroup.jsx
+++ src/screens/common/Inputs/CheckboxGroup.jsx
@@ -1,24 +1,20 @@
-import { useParams } from 'react-router-dom';
-
 import { EditableCheckbox } from '../EditableCheckbox';
 
-export const CheckboxGroup = ({ options, edit }) => {
-    const { jobId } = useParams();
-
-    return (
-        <fieldset className="flow flow-space-s">
-            {options.map((option) => {
-                return (
-                    <div key={option.id} className="activity" data-status="complete">
-                        <EditableCheckbox
-                            edit={edit}
-                            value={option.title}
-                            name="activity"
-                            link={`/jobs/${jobId}/activity/events/${option.id}`}
-                        />
-                    </div>
-                );
-            })}
-        </fieldset>
-    );
+export const CheckboxGroup = ({ options, edit, jobId }) => {
+  return (
+    <fieldset className="flow flow-space-s">
+      {options.map((option) => {
+        return (
+          <div key={option.id} className="activity" data-status="complete">
+            <EditableCheckbox
+              edit={edit}
+              value={option.title}
+              name="activity"
+              link={`/jobs/${jobId}/activity/events/${option.id}`}
+            />
+          </div>
+        );
+      })}
+    </fieldset>
+  );
 };
