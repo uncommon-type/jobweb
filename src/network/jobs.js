b32f0e3aff70b68b033c2a7710f2ac51374983c9
uncommon-type <uncommon.type@rambler.ru>
2024-02-26 21:08:35 +0300

    [#45] Add postJobActivity function responsible for creating new activity for specific job

--- src/network/jobs.js
+++ src/network/jobs.js
@@ -5,12 +5,12 @@ export const getJobs = async (token) => {
   return await getData(`${baseUrl}/jobs`, token, {});
 };
 
-export const postJob = async (jobToAdd, token) => {
-  return await sendData(`${baseUrl}/jobs`, jobToAdd, token);
-};
-
 export const getJob = async (jobId, token) => {
   return await getData(`${baseUrl}/jobs/${jobId}`, token, {});
+};
+
+export const postJob = async (jobToAdd, token) => {
+  return await sendData(`${baseUrl}/jobs`, jobToAdd, token);
 };
 
 export const updateJob = async (jobId, jobToUpdate, token) => {
@@ -19,4 +19,8 @@ export const updateJob = async (jobId, jobToUpdate, token) => {
 
 export const deleteJob = async (jobId, token) => {
   return await deleteData(`${baseUrl}/jobs/${jobId}`, token);
+};
+
+export const postJobActivity = async (activityToAdd, token, jobId) => {
+  return await sendData(`${baseUrl}/jobs/${jobId}/activities`, activityToAdd, token);
 };
