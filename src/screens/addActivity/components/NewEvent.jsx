8e51dbd044d70b5383f308340663b479072a7f1a
uncommon-type <uncommon.type@rambler.ru>
2024-02-27 13:10:25 +0300

    [#49] Add React Router's Form Component to <NewEvent/> and <NewTask/>
    - Use React Router's Form component in <NewEvent/> and <NewTask/> for
    handling form submissions
    
    - Specify method="post" for <Form/> component, indicating form will be
    submitted using HTTP POST method
    
    - Submit form to route that <AddActivity/> component is associated with

--- src/screens/addActivity/components/NewEvent.jsx
+++ src/screens/addActivity/components/NewEvent.jsx
@@ -1,39 +1,39 @@
+import { Form } from 'react-router-dom';
+
 import { EditableCheckbox } from '@screens/common/EditableCheckbox';
 import { EditableDateTime } from '@screens/common/EditableDateTime';
 import { EditableDescription } from '@screens/common/EditableDescription';
 import { SaveButton } from '@screens/common/Buttons/SaveButton';
 import { DeleteButton } from '@screens/common/Buttons/DeleteButton';
 
-export const NewEvent = ({ jobId }) => {
-  return (
-    <form className="flow">
-      <EditableCheckbox
-        edit={true}
-        showLabel={true}
-        label="Type (hiring call, interview, etc)"
-        name="activity"
-      />
-      <EditableDateTime
-        edit={true}
-        showLabel={true}
-        label="Start time"
-        value=""
-        name="date"
-      />
-      <EditableDescription
-        edit={true}
-        showLabel={true}
-        label="Description"
-        name="description"
-        value=""
-      />
-      <div className="sidebar">
-        <div />
-        <div className="cluster gap-left">
-          <SaveButton />
-          <DeleteButton />
-        </div>
+export const NewEvent = ({ jobId }) => (
+  <Form method="post" className="flow">
+    <EditableCheckbox
+      edit={true}
+      showLabel={true}
+      label="Type (hiring call, interview, etc)"
+      name="activity"
+    />
+    <EditableDateTime
+      edit={true}
+      showLabel={true}
+      label="Start time"
+      value=""
+      name="date"
+    />
+    <EditableDescription
+      edit={true}
+      showLabel={true}
+      label="Description"
+      name="description"
+      value=""
+    />
+    <div className="sidebar">
+      <div />
+      <div className="cluster gap-left">
+        <SaveButton />
+        <DeleteButton />
       </div>
-    </form>
-  );
-};
+    </div>
+  </Form>
+);
--- src/screens/addActivity/components/NewTask.jsx
+++ src/screens/addActivity/components/NewTask.jsx
@@ -1,31 +1,32 @@
+import { Form } from 'react-router-dom';
+
 import { EditableCheckbox } from '@screens/common/EditableCheckbox';
 import { EditableDescription } from '@screens/common/EditableDescription';
 import { SaveButton } from '@screens/common/Buttons/SaveButton';
 import { DeleteButton } from '@screens/common/Buttons/DeleteButton';
 
-export const NewTask = ({ jobId }) => {
-  return (
-    <form className="flow">
-      <EditableCheckbox
-        edit={true}
-        showLabel={true}
-        label="Short title"
-        name="activity"
-      />
-      <EditableDescription
-        edit={true}
-        showLabel={true}
-        label="Description"
-        name="description"
-        value=""
-      />
-      <div className="sidebar">
-        <div />
-        <div className="cluster gap-left">
-          <SaveButton />
-          <DeleteButton />
-        </div>
+export const NewTask = ({ jobId }) => (
+  <Form method="post" className="flow">
+    <EditableCheckbox
+      edit={true}
+      showLabel={true}
+      label="Short title"
+      name="activity"
+    />
+    <EditableDescription
+      edit={true}
+      showLabel={true}
+      label="Description"
+      name="description"
+      value=""
+    />
+    <div className="sidebar">
+      <div />
+      <div className="cluster gap-left">
+        <SaveButton />
+        <DeleteButton />
       </div>
-    </form>
-  );
-};
+    </div>
+  </Form>
+);
+
