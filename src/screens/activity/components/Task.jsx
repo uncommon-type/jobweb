ac67314d4135180701ca626122fccfcbc1226f69
uncommon-type <uncommon.type@rambler.ru>
2024-03-02 11:00:39 +0300

    [#60] Update setup of <Task/> component
    — Import Form from 'react-router-dom'
    
    — Wrap <Task/>'s' EditableCheckbox, EditableDescription components in
    React Router's <Form> element
    
    - Give <Form> element method="put" to indicate component is designed to
    handle updating event details on server
    
    - Add jobId and edit props to <Task/> component
    
    — Render <SaveButton/> and <DeleteButton/> within component when in edit
    mode
    
    - Use hidden inputs for sending data (job ID, activity ID) not directly
    entered by user but still necessary for processing submission of <Task/>
    form on server side

--- src/screens/activity/components/Task.jsx
+++ src/screens/activity/components/Task.jsx
@@ -1,9 +1,15 @@
+import { Form } from 'react-router-dom';
+
 import { EditableCheckbox } from '@screens/common/EditableCheckbox';
 import { EditableDescription } from '@screens/common/EditableDescription';
+import { DeleteButton } from '@screens/common/Buttons/DeleteButton';
+import { SaveButton } from '@screens/common/Buttons/SaveButton';
 
-export const Task = ({ activity, edit }) => {
+export const Task = ({ activity, edit, jobId }) => {
   return (
-    <>
+    <Form method="put" className="flow">
+      <input type="hidden" name="jobId" value={jobId} />
+      <input type="hidden" name="activityId" value={activity.id} />
       <EditableCheckbox
         edit={edit}
         label="Short title"
@@ -16,7 +22,16 @@ export const Task = ({ activity, edit }) => {
         value={activity.description}
         name="description"
       />
-    </>
+      {edit &&
+        <div className="sidebar">
+          <div />
+          <div className="cluster gap-left">
+            <SaveButton />
+            <DeleteButton />
+          </div>
+        </div>
+      }
+    </Form>
   );
 };
 
