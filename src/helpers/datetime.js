8aaf4fd11fe14f2b8f1bab5226c79bab834426e2
uncommon-type <uncommon.type@rambler.ru>
2024-07-08 20:15:20 +0300

    [#90] Update datetime.js
    - Add hour12 to timeOptions for 12-hour or 24-hour time formatting preference
    
    - Before using toLocaleDateString and Intl.DateTimeFormat, wrap  date param in new Date object to ensure proper date treatment regardless of input format

--- src/helpers/datetime.js
+++ src/helpers/datetime.js
@@ -1,16 +1,18 @@
 export const getDate = (date, { day = true, month = true, year = true } = {}) =>
-  date.toLocaleDateString('en-UK', {
+  new Date(date).toLocaleDateString('en-UK', {
     day: day ? 'numeric' : undefined,
     month: month ? 'long' : undefined,
     year: year ? 'numeric' : undefined
   });
 
-export const getTime = (date, { hour = true, minute = true } = {}) => {
+
+export const getTime = (date, { hour = true, minute = true, hour12 = true } = {}) => {
   const timeOptions = {
     hour: hour ? 'numeric' : undefined,
     minute: minute ? '2-digit' : undefined,
+    hour12: hour12,
   };
 
   const timeFormatter = new Intl.DateTimeFormat('en-UK', timeOptions);
-  return timeFormatter.format(date);
+  return timeFormatter.format(new Date(date));
 };
