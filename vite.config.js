4f98dbb4ca07224b68d4d5e6e6b821e1223d119c
uncommon-type <uncommon.type@rambler.ru>
2024-07-08 19:49:40 +0300

    [#89] Update Vite config file
    - Add PostCSS to Vite to ensure PostCSS transformations get automatically applied to CSS files during build process
    
    - Specify publicDir as '../public' to indicate where Vite serves static assets during development
    
    -Set outDir to '../dist' for asset placement post-build. Enable emptyOutDir: true to clear output directory before each build, avoiding outdated files

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,25 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig({
-  plugins: [react()],
-  root: 'src',
+import { postcss } from './postcss.config';
 
-  resolve: {
-    alias: {
-      '@screens': path.resolve(__dirname, 'src/screens'),
-      '@helpers': path.resolve(__dirname, 'src/helpers'),
-      '@network': path.resolve(__dirname, 'src/network'),
-      '@contexts': path.resolve(__dirname, 'src/contexts'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
+export default defineConfig({
+    plugins: [react()],
+    root: 'src',
+    
+    resolve: {
+        alias: {
+          '@screens': path.resolve(__dirname, 'src/screens'),
+          '@helpers': path.resolve(__dirname, 'src/helpers'),
+          '@hooks': path.resolve(__dirname, 'src/hooks'),
+          '@network': path.resolve(__dirname, 'src/network'),
+          '@contexts': path.resolve(__dirname, 'src/contexts'),
+        },
     },
-  },
-});
+    css: { postcss },
+    publicDir: '../public',
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true,
+    }
+})
