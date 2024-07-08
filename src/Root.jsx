fe6f1dc6cf50be05188028fa1cec05d3c645d849
uncommon-type <uncommon.type@rambler.ru>
2024-07-08 20:36:34 +0300

    [#91] Update <Root/> component to use shorthand syntax for single-expression functions, omitting curly braces {} and return keyword

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,10 +1,8 @@
 import { Outlet } from 'react-router-dom';
 
-export const Root = () => {
-  return (
-    <div className="wrapper decor">
-      <span className="pin"></span>
+export const Root = () => (
+    <div className='wrapper decor'>
+      <span className='pin'></span>
       <Outlet />
     </div>
-  );
-};
+);
