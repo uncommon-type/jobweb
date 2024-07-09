a7af42c9cbe05daa12b579743ae06d5bc80f8aba
uncommon-type <uncommon.type@rambler.ru>
2024-07-09 18:08:55 +0300

    [#95] Update routing configuration
    - Update routing setup, focusing on nested routes. Implement deeper nesting  for 'jobs/:jobId/*' path, integrating 'JobContent' route alongside nested paths ‘for role', 'company', 'activity', and 'offer'
    
    - Implement catch-all route at the end of both root and jobs/:jobId/ paths' children arrays to capture unmatched URLs and show <NotFound/> component
    
    - Wire up destroy action to ‘/jobs’ route

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,16 +4,17 @@ import { Root } from './Root';
 import { Error } from '@screens/common/Error/Error';
 import { LoginSignup, action as loginAction, loader as loginLoader } from '@screens/loginSignup/LoginSignup';
 import { action as signupAction } from '@screens/loginSignup/components/Signup/SignupForm';
-import { Jobs, loader as jobsLoader } from '@screens/jobs/Jobs';
+import { Jobs, loader as jobsLoader, action as destroyAction } from '@screens/jobs/Jobs';
 import { Job, loader as jobLoader } from '@screens/job/Job';
 import { JobContent } from '@screens/job/components/JobContent';
 import { RoleInfo } from '@screens/job/components/RoleInfo/RoleInfo';
 import { CompanyInfo } from '@screens/job/components/CompanyInfo/CompanyInfo';
-import { ActivityInfo, action as updateActivityAction } from '@screens/job/components/ActivityInfo';
+import { ActivityInfo, action as modifyActivityInfoAction } from '@screens/job/components/ActivityInfo';
 import { OfferInfo } from '@screens/job/components/OfferInfo/OfferInfo';
 import { AddJob, action as addJobAction } from '@screens/addJob/AddJob';
-import { Activity, loader as activityLoader, action as activityAction } from '@screens/activity/Activity';
-import { AddActivity, loader as addActivityLoader, action as addActivityAction } from '@screens/addActivity/AddActivity';
+import { Activity, action as activityAction } from '@screens/activity/Activity';
+import { AddActivity, action as addActivityAction } from '@screens/addActivity/AddActivity';
+import { NotFound } from '@screens/notFound/NotFound';
 
 const routes = [
   {
@@ -36,6 +37,7 @@ const routes = [
         path: 'jobs',
         element: <Jobs />,
         loader: jobsLoader,
+        action: destroyAction,
       },
       {
         path: 'jobs/new',
@@ -44,49 +46,46 @@ const routes = [
       },
       {
         path: 'jobs/:jobId/*',
+        element: <Job />,
+        loader: jobLoader,
+        id: 'job',
         children: [
           {
-            element: <Job />,
-            loader: jobLoader,
+            element: <JobContent />,
             children: [
               { path: 'role', element: <RoleInfo /> },
               { path: 'company', element: <CompanyInfo /> },
-              { path: 'activity', element: <ActivityInfo />, action: updateActivityAction },
-              { path: 'offer', element: <OfferInfo /> },
+              {
+                path: 'activity', element: <ActivityInfo />,
+                action: modifyActivityInfoAction 
+              },
+              { path: 'offer', element: <OfferInfo /> }
             ],
-          },
-          {
-            path: '',
-            element: <JobContent />,
-            loader: jobLoader,
-
           },
           {
             path: 'activity/events/:activityId',
             element: <Activity />,
-            loader: activityLoader,
             action: activityAction
           },
           {
             path: 'activity/tasks/:activityId',
             element: <Activity />,
-            loader: activityLoader,
-            action: activityAction
+            action: activityAction 
           },
           {
             path: 'activity/events/new',
             element: <AddActivity isEvent={true} />,
-            loader: addActivityLoader,
             action: addActivityAction,
           },
           {
             path: 'activity/tasks/new',
             element: <AddActivity isEvent={false} />,
-            loader: addActivityLoader,
             action: addActivityAction,
           },
+          { path: '*', element: <NotFound /> },
         ],
       },
+      { path: '*', element: <NotFound /> },
     ],
   },
   {
