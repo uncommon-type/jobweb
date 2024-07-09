0c91d4bbbfab31defc581248c36f500a9d7e138b
uncommon-type <uncommon.type@rambler.ru>
2024-07-09 10:55:19 +0300

    [#94] Add error handling to <Input/> component to enhance its functionality
    - Introduce  error prop. Ensure that if provided, it displays < Alert/> component with error message

--- src/screens/common/Inputs/Input.jsx
+++ src/screens/common/Inputs/Input.jsx
@@ -1,25 +1,31 @@
 import { useId } from 'react';
 
-export const Input = ({ label, value, name, id, showLabel }) => {
+import { Alert } from '../Alert';
+
+export const Input = ({ label, value, name, id, showLabel, error }) => {
   const generatedId = useId();
   const appliedId = id || generatedId;
 
   return (
-    <div className="form-item">
-      <label htmlFor={`${name}-${appliedId}`} className="font-special">
-        {showLabel ? (
-          label
-        ) : (
-          <span className="sr-only">{`Edit ${name} name`}</span>
-        )}
-      </label>
-      <input
-        type="text"
-        id={`${name}-${appliedId}`}
-        name={name}
-        defaultValue={value}
-        className="width-20"
-      />
-    </div>
+      <div className='form-item'>
+        <label htmlFor={`${name}-${appliedId}`} className='font-special'>
+          {showLabel ? (
+            label
+          ) : (
+            <span className='sr-only'>{`Edit ${name} name`}</span>
+          )}
+
+          <div>
+            <input
+              type='text'
+              id={`${name}-${appliedId}`}
+              name={name}
+              defaultValue={value}
+              className='width-20'
+            />
+            {error && <Alert>{error}</Alert>}
+          </div>
+        </label>
+      </div>
   );
 };
