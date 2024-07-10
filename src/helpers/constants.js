1003e86a8650d5eaa984fac30cc3e6dd5fe730fa
uncommon-type <uncommon.type@rambler.ru>
2024-07-10 14:28:13 +0300

    [#96] Manage app's links and URLs in centralised maintainable way
    - Store links and URLs in central location
    
    - Create endpoints utility object for endpoints used for making API calls
    to server
    
    - Create links utility object for links intended for navigation within
    app

---
+++ src/helpers/constants.js
@@ -0,0 +1,35 @@
+export const SERVICE_URL = `http://localhost:3000`;
+
+export const endpoints = {
+    login: `${SERVICE_URL}/login`,
+    jobs: {
+        list: `${SERVICE_URL}/jobs`,
+        find: (jobId) => `${SERVICE_URL}/jobs/${jobId}`,
+        activities: {
+          add: (jobId) => `${SERVICE_URL}/jobs/${jobId}/activities`,
+          update: (jobId, activityId) => `${SERVICE_URL}/jobs/${jobId}/activities/${activityId}`,
+          delete: (jobId, activityId) => `${SERVICE_URL}/jobs/${jobId}/activities/${activityId}`
+        }
+    },
+    users: {
+      create: `${SERVICE_URL}/user`
+    }
+};
+
+export const links = {
+  login: '/',
+  signIn: '/sign-in',
+  jobs: {
+    list: '/jobs',
+    new: '/jobs/new',
+    role: (jobId) => `/jobs/${jobId}/role`,
+    activity: {
+      events: {
+        new: (jobId) => `/jobs/${jobId}/activity/events/new`
+      },
+      tasks: {
+        new: (jobId) => `/jobs/${jobId}/activity/tasks/new`
+      }
+    }
+  }
+};
