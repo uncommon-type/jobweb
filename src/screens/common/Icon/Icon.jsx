fa09ab70ef4c4a0ae7bd8d3d3a4fd04267fcee50
uncommon-type <uncommon.type@rambler.ru>
2024-07-15 15:51:52 +0300

    [#99] Implement <Icon/> component
    - Add app icons, ensure decorative icons are not selectable via keyboard
    navigation
    
    - Create ICON_MAP object, linking icon names with their corresponding
    icons
    
    -Create <Icon/> component that uses icon prop to select appropriate icon
    for display
    
    - Apply any extra props passed to <Icon/> to customise its look or
    behaviour

---
+++ src/screens/common/Icon/Icon.jsx
@@ -0,0 +1,14 @@
+// import { ArrowIcon } from './images/ArrowIcon';
+import { ArrowLongRightIcon as  ArrowIcon} from '@heroicons/react/24/outline';
+import { SpinnerIcon } from './images/SpinnerIcon';
+
+const ICON_MAP = {
+  arrow: ArrowIcon,
+  spinner: SpinnerIcon,
+};
+
+export const Icon = ({ icon = '', ...props }) => {
+  const IconComponent = ICON_MAP[icon];
+
+  return <IconComponent {...props} />;
+};
---
+++ src/screens/common/Icon/images/SpinnerIcon.jsx
@@ -0,0 +1,20 @@
+export const SpinnerIcon = ({ ...props }) => (
+  <svg
+    aria-hidden='true'
+    focusable='false'
+    viewBox='0 0 34 34'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+    className='spin'
+    {...props}>
+    <g>
+      <circle cx='17' cy='17' r='15' strokeWidth='4' />
+      <path
+        d='M32 17C32 8.71573 25.2843 2 17 2'
+        stroke='white'
+        strokeWidth='4'
+        strokeLinecap='round'
+      />
+    </g>
+  </svg>
+);
