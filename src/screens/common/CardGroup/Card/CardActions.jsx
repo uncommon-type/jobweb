36266679d5f29a9e4bbb4615395e9deb7da24600
uncommon-type <uncommon.type@rambler.ru>
2024-07-15 16:11:09 +0300

    [#103] Update approach to rendering buttons and navigation links in <CardActions/>
    - Use custom <Button/> component for both 'View' and 'Delete' actions

--- src/screens/common/CardGroup/Card/CardActions.jsx
+++ src/screens/common/CardGroup/Card/CardActions.jsx
@@ -1,20 +1,8 @@
-import { Link } from 'react-router-dom';
+import { Button } from '@screens/common/Buttons/Button';
 
-export const CardActions = ({ jobId }) => {
-  return (
-    <div className="cluster">
-      <Link
-        to={`${jobId}/role`}
-        className="button"
-        data-type="secondary"
-        aria-label="View job"
-      >
-        View
-      </Link>
-
-      <button className="button" data-type="secondary">
-        Remove
-      </button>
-    </div>
-  );
-};
+export const CardActions = ({ jobId, ...otherProps }) => (
+  <div className='cluster'>
+    <Button to={`${jobId}/role`} label='View' aria-label='View job' variant='secondary' />
+    <Button label='Delete' aria-label='Delete job' variant='secondary'id={jobId} {...otherProps} />
+  </div>
+);
