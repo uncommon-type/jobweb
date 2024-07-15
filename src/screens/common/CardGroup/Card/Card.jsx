3f37660cdfd008a64b2289d2475f73652b594ac3
uncommon-type <uncommon.type@rambler.ru>
2024-07-15 16:10:18 +0300

    [#102] Enhance <Card/> component's flexibility by adding date property and allowing additional props to be passed to <CardActions/>

--- src/screens/common/CardGroup/Card/Card.jsx
+++ src/screens/common/CardGroup/Card/Card.jsx
@@ -1,21 +1,22 @@
 import { CardActions } from './CardActions';
 
-export const Card = ({ className = '', job, showButtons }) => {
-  const { company, status, jobTitle, id } = job;
+export const Card = ({ className = '', job, showButtons, ...otherProps }) => {
+  const { company, status, jobTitle, date, id } = job;
   const appliedClass = `card sidebar ${className}`;
 
   return (
     <article className={appliedClass}>
-      <div className="box" />
-      <div className="card__content flow">
+      <div className='box' />
+      <div className='card__content flow'>
         <div>
           <p>{company?.name}</p>
-          <p className="text-500">{jobTitle}</p>
+          <p className='text-500'>{jobTitle}</p>
           <div>
-            <span className="meta-item">{status}</span>
+            <span className='meta-item'>{status}</span>
+            <span className='meta-item'>{date}</span>
           </div>
         </div>
-        {showButtons ? <CardActions jobId={id} /> : null}
+        {showButtons ? <CardActions jobId={id} {...otherProps} /> : null}
       </div>
     </article>
   );
