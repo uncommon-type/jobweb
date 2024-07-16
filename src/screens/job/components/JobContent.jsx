2bf58ca5fbccaecc86417b790d3268111bf57277
uncommon-type <uncommon.type@rambler.ru>
2024-07-16 11:37:11 +0300

    [#111] Use useOutletContext to directly access and manage context values from <Outlet/> context, removing need to manually pass them down through props

--- src/screens/job/components/JobContent.jsx
+++ src/screens/job/components/JobContent.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react';
-import { useLocation, Outlet } from 'react-router-dom';
+import { useLocation, Outlet, useOutletContext } from 'react-router-dom';
 
 import { Header } from '@screens/common/Header/Header';
 import { SecondaryNav } from '@screens/common/Header/SecondaryNav';
 import { Card } from '@screens/common/CardGroup/Card/Card';
 import { JobTabs } from './JobTabs';
 
-export const JobContent = ({ from, job }) => {
-  const [edit, setEdit] = useState(false);
+export const JobContent = () => {
+  const { from, job, edit, setEdit } = useOutletContext();
   const location = useLocation();
 
   const handleEdit = () => {
@@ -24,10 +23,10 @@ export const JobContent = ({ from, job }) => {
         />
       </Header>
       <main>
-        <section className="job-details-group flow">
-          <Card job={job} className="no-border" />
+        <section className='job-details-group flow'>
+          <Card job={job} className='no-border' />
           <JobTabs onChange={() => setEdit(false)} jobStatus={job?.status} />
-          <Outlet context={{ job, edit }} />
+          <Outlet context={{ job, edit, setEdit }} />
         </section>
       </main>
     </>
