213a7c2df1bc06e2bb80ad3da5387ddcae66862a
uncommon-type <uncommon.type@rambler.ru>
2024-07-16 13:40:05 +0300

    [#114] Implement handling of form validation errors
    - Add handling of validation errors
    
    - Store validation errors in actionData.errors and pass them down to
    <NewEvent/> and < NewTask/> components to provide feedback to users about
    what needs to be corrected in their form submissions
    
    - Leverage React Router's <Form/> component for form submission

--- src/screens/addActivity/AddActivity.jsx
+++ src/screens/addActivity/AddActivity.jsx
@@ -1,38 +1,17 @@
-import { useState } from 'react';
-import { useLoaderData, redirect, useActionData } from 'react-router-dom';
+import { redirect, useActionData, useOutletContext, Form } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 
 import { authenticate } from '@helpers/token';
-import { getJob, postJobActivity } from '@network/jobs';
-import { validateActivity } from './components/helpers/validate-activity';
+import { postJobActivity } from '@network/jobs';
+import { validateActivity } from './helpers/validate-activity';
 
 import { Header } from '@screens/common/Header/Header';
 import { SecondaryNav } from '@screens/common/Header/SecondaryNav';
 import { NewActivityTabs } from './components/NewActivityTabs';
 import { NewEvent } from './components/NewEvent';
 import { NewTask } from './components/NewTask';
-import { Alert } from '@screens/common/Alert';
 
-export const loader = async ({ params }) => {
-  const token = authenticate();
-
-  if (!token) {
-    return redirect('/');
-  }
-
-  try {
-    return await getJob(params.jobId, token);
-  } catch (err) {
-    console.error('Error caught while attempting to fetch a job in activity loader', err);
-    if (err.status !== 404) {
-      throw new Response('', { status: err.status || 500, statusText: 'Something went wrong' })
-    }
-
-    throw new Response('', { status: err.status, statusText: 'Job not found' });
-  }
-};
-
-export const action = async ({ request, params }) => {
+export const action = async ({ request }) => {
   const token = authenticate();
 
   if (!token) {
@@ -42,50 +21,53 @@ export const action = async ({ request, params }) => {
   const formData = await request.formData();
 
   const {
-    jobId, type, activityInput: activityTitle, time, date, description, activity: activityCheckbox
+    jobId,
+    type,
+    activityTitle,
+    activityStatus = false,
+    time,
+    date,
+    description,
   } = Object.fromEntries(formData);
 
   const activityToAdd = {
     id: uuidv4(),
     type,
-    done: activityCheckbox === "on" ? true : false
+    done: activityStatus ? true : false,
   };
 
   if (activityTitle) {
-    activityToAdd.title = activityTitle
+    activityToAdd.title = activityTitle;
   }
 
   if (description) {
-    activityToAdd.description = description
+    activityToAdd.description = description;
   }
 
   if (time && date) {
-    activityToAdd.startDate = new Date(`${date} ${time}`).toISOString()
+    activityToAdd.startDate = new Date(`${date} ${time}`).toISOString();
   }
 
   const errors = validateActivity(activityToAdd);
 
   if (errors.length !== 0) {
-    return errors;
+    return { errors };
   }
 
   try {
     await postJobActivity(activityToAdd, token, jobId);
     return redirect(`/jobs/${jobId}/activity`);
-  } catch (err) {
-    console.error('Error caught while attempting to post an activity in activity action)', err);
+  }
+  catch (err) {
+    console.error('Error caught while attempting to post an activity in activity action', err);
     throw new Response('', { status: err.status || 500, statusText: 'Something went wrong' });
   }
 };
 
 export const AddActivity = ({ isEvent = false }) => {
-  const [activityCheckbox, setActivityCheckbox] = useState(false);  ///////???
-  const job = useLoaderData();
-  const actionData = useActionData() || [];
-
-  const handleChange = async (e) => {
-    setActivityCheckbox(e.target.checked);
-  }
+  const { job } = useOutletContext();
+  const actionData = useActionData() || {};
+  const errors = actionData?.errors || [];
 
   return (
     <>
@@ -93,15 +75,15 @@ export const AddActivity = ({ isEvent = false }) => {
         <SecondaryNav fromLink={`/jobs/${job.id}/activity`} />
       </Header>
       <main>
-        <section className="add-activity-group flow">
+        <section className='add-activity-group flow'>
           <NewActivityTabs jobId={job.id} />
-          {isEvent ? <NewEvent jobId={job.id} onChange={handleChange} activityCheckbox={activityCheckbox} /> : <NewTask jobId={job.id} onChange={handleChange} activityCheckbox={activityCheckbox} />}
-          {actionData.length !== 0
-            ? actionData.map((error) => <Alert key={error.message} message={error.message} />)
-            : null}
+          <Form method='post' className='flow' noValidate>
+            {isEvent
+              ? <NewEvent jobId={job.id} errors={errors} />
+              : <NewTask jobId={job.id} errors={errors} />}
+          </Form>
         </section>
       </main>
     </>
   );
 };
-
