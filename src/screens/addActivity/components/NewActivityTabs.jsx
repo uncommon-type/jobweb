f9cb249a9c71e45a049c77c81b9a0e3fd28d402d
uncommon-type <uncommon.type@rambler.ru>
2024-07-16 13:43:10 +0300

    [#116]  Generate paths for navigation links by using preconfigured links from links utility object

--- src/screens/addActivity/components/NewActivityTabs.jsx
+++ src/screens/addActivity/components/NewActivityTabs.jsx
@@ -1,17 +1,19 @@
 import { NavLink } from 'react-router-dom';
 
+import { links } from '@helpers/constants';
+
 export const NewActivityTabs = ({ jobId }) => {
   const NAVLINKS = [
-    { label: 'Event', path: `/jobs/${jobId}/activity/events/new` },
-    { label: 'Task', path: `/jobs/${jobId}/activity/tasks/new` },
+    { label: 'Event', path: links.jobs.activity.events.new(jobId) },
+    { label: 'Task', path: links.jobs.activity.tasks.new(jobId) },
   ];
 
   return (
-    <nav className="activity-tabs">
-      <ul role="list" className="activity-tab__list cluster">
+    <nav className='activity-tabs'>
+      <ul role='list' className='activity-tab__list cluster'>
         {NAVLINKS.map(({ label, path }) => (
           <li key={label}>
-            <NavLink to={path} aria-label={`View ${label}`} className="title">
+            <NavLink to={path} aria-label={`View ${label}`} className='title'>
               {label}
             </NavLink>
           </li>
