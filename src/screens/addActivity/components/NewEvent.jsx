8415e8c9829bf6427b84bd14adda8d526573dcfc
uncommon-type <uncommon.type@rambler.ru>
2024-07-16 13:41:08 +0300

    [#115] Enhance component's interactivity, error handling and flexibility
    - Add state management for tracking checkbox state and include logic to
    update checkbox state upon user interaction
    
    - Add error handling by incorporating errors prop and displaying error
    messages for specific fields, using getErrorMessage function
    
    - Replace <SaveButton/> and <DeleteButton/> with custom <Button/> component
    
    - Conditionally render <Alert/> component to display error messages
    related to description field

--- src/screens/addActivity/components/NewEvent.jsx
+++ src/screens/addActivity/components/NewEvent.jsx
@@ -1,44 +1,68 @@
-import { Form } from 'react-router-dom';
+import { useState } from 'react';
 
 import { EditableCheckbox } from '@screens/common/EditableCheckbox';
 import { EditableDateTime } from '@screens/common/EditableDateTime';
 import { EditableDescription } from '@screens/common/EditableDescription';
-import { SaveButton } from '@screens/common/Buttons/SaveButton';
-import { DeleteButton } from '@screens/common/Buttons/DeleteButton';
+import { Alert } from '@screens/common/Alert';
 
-export const NewEvent = ({ jobId, onChange, activityCheckbox }) => (
-  <Form method="post" className="flow">
-    <input type="hidden" name="jobId" value={jobId} />
-    <input type="hidden" name="type" value="event" />
-    <EditableCheckbox
-      edit={true}
-      showLabel={true}
-      label="Type (hiring call, interview, etc)"
-      name="activity"
-      className="items-end"
-      checked={activityCheckbox}
-      onChange={onChange}
-    />
-    <EditableDateTime
-      edit={true}
-      showLabel={true}
-      label="Start time"
-      value=""
-      name="date"
-    />
-    <EditableDescription
-      edit={true}
-      showLabel={true}
-      label="Description"
-      name="description"
-      value=""
-    />
-    <div className="sidebar">
-      <div />
-      <div className="cluster gap-left">
-        <SaveButton />
-        <DeleteButton />
+const getErrorMessage = (erroredFields, field) => {
+  const error = erroredFields.find(erroredField => erroredField.name === field);
+  return error ? error.message : null;
+};
+
+export const NewEvent = ({ jobId, errors }) => {
+  const [isChecked, setIsChecked] = useState(false); /// / new
+
+  const descriptionError = getErrorMessage(errors, 'description');
+  const activityTitleError = getErrorMessage(errors, 'activity');
+
+  return (
+    <>
+      <input type='hidden' name='jobId' value={jobId} />
+      <input type='hidden' name='type' value='event' />
+      <EditableCheckbox
+        edit={true}
+        showLabel={true}
+        label='Type (hiring call, interview, etc)'
+        name='activity'
+        className='items-end'
+        error={activityTitleError}
+        checked={isChecked}
+        onChange={(e) => {
+          setIsChecked(e.target.checked);
+        }}
+      />
+      <EditableDateTime
+        edit={true}
+        showLabel={true}
+        label='Start time'
+        value=''
+        name='date'
+      />
+      <EditableDescription
+        edit={true}
+        showLabel={true}
+        label='Description'
+        value=''
+        name='description'
+      />
+      {descriptionError && (
+        <Alert>
+          {' '}
+          {descriptionError}
+        </Alert>
+      )}
+      <div className='sidebar'>
+        <div />
+        <div className='cluster gap-left'>
+          <button type='submit' className='button' data-type='primary'>
+            Save
+          </button>
+          <button className='button' data-type='primary'>
+            Cancel
+          </button>
+        </div>
       </div>
-    </div>
-  </Form>
-);
+    </>
+  );
+};
--- src/screens/addActivity/components/NewTask.jsx
+++ src/screens/addActivity/components/NewTask.jsx
@@ -1,37 +1,60 @@
-import { Form } from 'react-router-dom';
+import { useState } from 'react';
 
 import { EditableCheckbox } from '@screens/common/EditableCheckbox';
 import { EditableDescription } from '@screens/common/EditableDescription';
-import { SaveButton } from '@screens/common/Buttons/SaveButton';
-import { DeleteButton } from '@screens/common/Buttons/DeleteButton';
+import { Alert } from '@screens/common/Alert';
 
-export const NewTask = ({ jobId, onChange, activityCheckbox }) => (
-  <Form method="post" className="flow">
-    <input type="hidden" name="jobId" value={jobId} />
-    <input type="hidden" name="type" value="task" />
-    <EditableCheckbox
-      edit={true}
-      showLabel={true}
-      label="Short title"
-      name="activity"
-      className="items-end"
-      checked={activityCheckbox}
-      onChange={onChange}
-    />
-    <EditableDescription
-      edit={true}
-      showLabel={true}
-      label="Description"
-      name="description"
-      value=""
-    />
-    <div className="sidebar">
-      <div />
-      <div className="cluster gap-left">
-        <SaveButton />
-        <DeleteButton />
+const getErrorMessage = (erroredFields, field) => {
+  const error = erroredFields.find(erroredField => erroredField.name === field);
+  return error ? error.message : null;
+};
+
+export const NewTask = ({ jobId, errors }) => {
+  const [isChecked, setIsChecked] = useState(false); /// / new
+  const descriptionError = getErrorMessage(errors, 'description');
+  const activityTitleError = getErrorMessage(errors, 'activity');
+
+  return (
+    <>
+      <input type='hidden' name='jobId' value={jobId} />
+      <input type='hidden' name='type' value='task' />
+      <EditableCheckbox
+        edit={true}
+        showLabel={true}
+        label='Short title'
+        name='activity'
+        className='items-end'
+        error={activityTitleError}
+        checked={isChecked} // new
+        onChange={(e) => {
+          setIsChecked(e.target.checked);
+        }}
+
+      />
+      <EditableDescription
+        edit={true}
+        showLabel={true}
+        label='Description'
+        name='description'
+        value='' //  "NaN characters left"
+      />
+      {descriptionError && (
+        <Alert>
+          {' '}
+          {descriptionError}
+        </Alert>
+      )}
+      <div className='sidebar'>
+        <div />
+        <div className='cluster gap-left'>
+          <button type='submit' className='button' data-type='primary'>
+            Save
+          </button>
+          <button className='button' data-type='primary'>
+            Cancel
+          </button>
+        </div>
       </div>
-    </div>
-  </Form>
-);
-
+    </>
+  );
+};
