d277f6eb8b4943db0862fd7ca749fc1385cff2dc
uncommon-type <uncommon.type@rambler.ru>
2024-07-16 13:17:01 +0300

    [#113] Add onRemove prop to handle removal of job activities represented by checkboxes along with onClick event handler that triggers  onRemove function

--- src/screens/common/Inputs/CheckboxGroup.jsx
+++ src/screens/common/Inputs/CheckboxGroup.jsx
@@ -2,27 +2,25 @@ import { TrashIcon } from '@heroicons/react/24/outline';
 
 import { EditableCheckbox } from '../EditableCheckbox';
 
-export const CheckboxGroup = ({ options, edit, jobId, onChange }) => {
-  return (
-    <fieldset className="flow flow-space-s option-list">
-      {options.map(({ id, title, done }) => {
-        return (
-          <div key={id} className="cluster option-list__inner">
-            <EditableCheckbox key={id}
-              edit={edit}
-              name={title}
-              checked={done}
-              value={title}
-              onChange={onChange}
-              id={id}
-              link={`/jobs/${jobId}/activity/events/${id}`}
-            />
-            <button className="cluster option-button" aria-label="Delete item">
-              <TrashIcon id={id} />
-            </button>
-          </div>
-        );
-      })}
-    </fieldset >
-  );
-};
+export const CheckboxGroup = ({ options, edit, jobId, onChange, onRemove }) => (
+  <fieldset className='flow flow-space-xl option-list'>
+    {options.map(({ id, title, done }) => {
+      return (
+        <div key={id} className='cluster option-list__inner'>
+          <EditableCheckbox
+            edit={edit}
+            name={title}
+            checked={done}
+            value={title}
+            onChange={onChange}
+            id={id}
+            link={`/jobs/${jobId}/activity/events/${id}`}
+          />
+          <button className='cluster option-button' aria-label='Delete item'>
+            <TrashIcon onClick={onRemove} id={id} />
+          </button>
+        </div>
+      );
+    })}
+  </fieldset>
+);
