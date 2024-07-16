32129f28d6b0c5912c73b3f00ae3d443bbe9fd2a
uncommon-type <uncommon.type@rambler.ru>
2024-07-16 11:36:35 +0300

    [#110] Add state management for toggling edit mode and add <Outlet/> for dynamic content rendering

--- src/screens/job/Job.jsx
+++ src/screens/job/Job.jsx
@@ -1,10 +1,8 @@
-import { useLoaderData, useLocation, redirect } from 'react-router-dom';
+import { useState } from 'react';
+import { useLocation, useLoaderData, redirect, Outlet } from 'react-router-dom';
 
 import { authenticate } from '@helpers/token';
 import { getJob } from '@network/jobs';
-
-import { JobContent } from './components/JobContent';
-
 
 export const loader = async ({ params }) => {
   const token = authenticate();
@@ -15,21 +13,23 @@ export const loader = async ({ params }) => {
 
   try {
     return await getJob(params.jobId, token);
-  } catch (err) {
+  }
+  catch (err) {
     console.error('Error fetching a job in job loader', err);
     throw new Response('', {
       status: err.status || 500,
-      statusText: err.statusText || 'Something went wrong'
-    })
+      statusText: err.statusText || 'Something went wrong',
+    });
   }
-}
+};
 
 export const Job = () => {
+  const [edit, setEdit] = useState(false);
   const job = useLoaderData();
   const location = useLocation();
   const from = location.state?.from || '/jobs';
 
   return (
-    <JobContent from={from} job={job} />
+    <Outlet context={{ job, from, edit, setEdit }} />
   );
 };
