bd3a38452021011d1c17cef21ef0a4f941c8aa95
uncommon-type <uncommon.type@rambler.ru>
2024-07-16 10:03:29 +0300

    [#105] Update outer div class name in <App/> component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import { RouterProvider} from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 
-import { router } from "./routes";
+import { router } from './routes';
 import { Root } from './Root';
 
 export const App = () => (
-    <div className='k'>
-      <RouterProvider router={router} fallbackElement={<Root />} />
-    </div>
-)
+  <div className='app__inner'>
+    <RouterProvider router={router} fallbackElement={<Root />} />
+  </div>
+);
