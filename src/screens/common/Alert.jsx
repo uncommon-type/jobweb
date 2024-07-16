8b0dd3dbc1b2b016e66bf1e1c9464ab157512dd3
uncommon-type <uncommon.type@rambler.ru>
2024-07-16 10:07:23 +0300

    [#106] Remove unnecessary return statement

--- src/screens/common/Alert.jsx
+++ src/screens/common/Alert.jsx
@@ -1,10 +1,8 @@
 import { ExclamationTriangleIcon as AlertIcon } from '@heroicons/react/24/outline';
 
-export const Alert = ({ message }) => {
-  return (
-    <figure className="alert cluster">
-      <AlertIcon />
-      <p>{message}</p>
-    </figure>
-  );
-};
+export const Alert = ({ message }) => (
+  <figure className='alert cluster'>
+    <AlertIcon />
+    <p>{message}</p>
+  </figure>
+);
