b1679040074ffbb077298c8b8aa1eea1da960109
uncommon-type <uncommon.type@rambler.ru>
2024-07-17 10:00:38 +0300

    [#121] Enhance <NotFound/> component with dynamic redirection and interactive button
    - Add 'Go back' button
    
    - Add dynamic redirection functionality, using react-router-dom's
    useLocation hook, to allow users to be redirected either back to their
    previous location or to default route (login page)

--- src/screens/notFound/NotFound.jsx
+++ src/screens/notFound/NotFound.jsx
@@ -1,3 +1,25 @@
+import { useLocation } from 'react-router-dom';
+
+import { Button } from '@screens/common/Buttons/Button';
+import { links } from '@helpers/constants';
+
 export const NotFound = () => {
-  return <h1>NotFound View</h1>;
+  const location = useLocation();
+  const from = location.state?.from || `${links.login}`;
+
+  return (
+    <main className='flow'>
+      <h1>404</h1>
+      <div className='flow'>
+        <p>We&apos;re sorry but the page you&apos;re looking for can&apos;t be found.</p>
+        <Button
+          to={from}
+          className='with-icon'
+          icon='arrow'
+          label='Go back'
+          aria-label='Go back'
+        />
+      </div>
+    </main>
+  );
 };
