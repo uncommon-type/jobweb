0a8670744da5b8103c07222c56906db54a3ff4a1
uncommon-type <uncommon.type@rambler.ru>
2024-07-17 10:55:28 +0300

    [#123] Update how <Activity/> component fetches, manages data
    - Use useOutletContext() to access context data passed down from parent
    components via React Router's outlet context
    
    - Use local state management for toggling between viewing and editing modes

--- src/screens/activity/Activity.jsx
+++ src/screens/activity/Activity.jsx
@@ -1,46 +1,14 @@
 import { useState } from 'react';
-import { redirect, useLoaderData, useActionData, useNavigate } from 'react-router-dom';
+import { redirect, useOutletContext, useParams } from 'react-router-dom';
 
 import { authenticate } from '@helpers/token';
-import { getJob } from '@network/jobs';
+import { validateActivity } from './helpers/validate-act';
 import { updateJobActivity } from '@network/jobs';
-import { validateActivity } from '@screens/addActivity/components/helpers/validate-activity';
 
 import { Header } from '@screens/common/Header/Header';
 import { SecondaryNav } from '@screens/common/Header/SecondaryNav';
 import { Event } from './components/Event';
 import { Task } from './components/Task';
-
-export const loader = async ({ params }) => {
-  const token = authenticate();
-
-  if (!token) {
-    return redirect('/');
-  }
-
-  try {
-    const job = await getJob(params.jobId, token);
-
-    const activity = job.activities.find(
-      (activity) => activity.id === params.activityId,
-    );
-
-    if (!activity) {
-      throw new Response('', {
-        status: 404,
-        statusText: 'Activity not found',
-      });
-    }
-
-    return { job, activity };
-  } catch (err) {
-    console.error('Error caught while attempting to fetch job', err);
-    throw new Response('', {
-      status: err.status,
-      statusText: err.statusText || 'Something went wrong'
-    })
-  }
-};
 
 export const action = async ({ request, params }) => {
   const token = authenticate();
@@ -52,29 +20,25 @@ export const action = async ({ request, params }) => {
   const { activityId, jobId } = params;
   const formData = await request.formData();
 
-  const {
-    activityInput: activityTitle,
-    activity: activityCheckbox,
-    time,
-    date,
-    description,
-  } = Object.fromEntries(formData);
+  const { activityStatus, type, activityTitle, time, date, description }
+    = Object.fromEntries(formData);
 
   const activityToUpdate = {
     id: activityId,
-    done: activityCheckbox === "on" ? true : false
-  }
+    type,
+    done: activityStatus && activityStatus === 'on' ? true : false,
+  };
 
   if (activityTitle) {
-    activityToUpdate.title = activityTitle
+    activityToUpdate.title = activityTitle;
   }
 
   if (time && date) {
-    activityToUpdate.startDate = new Date(`${date} ${time}`).toISOString()
+    activityToUpdate.startDate = new Date(`${date} ${time}`).toISOString();
   }
 
   if (description) {
-    activityToUpdate.description = description
+    activityToUpdate.description = description;
   }
 
   const errors = validateActivity(activityToUpdate);
@@ -84,38 +48,28 @@ export const action = async ({ request, params }) => {
   }
 
   try {
-    await updateJobActivity(activityToUpdate, token, jobId);
-    return redirect(`/jobs/${jobId}/activity`);
-  } catch (err) {
-    console.error('Error caught while attempting to update an activity in activity action)', err);
+    return await updateJobActivity(activityToUpdate, token, jobId);
+  }
+  catch (err) {
+    console.error(
+      'Error caught while attempting to update an activity in activity action)',
+      err,
+    );
     throw new Response('', {
       status: err.status,
-      statusText: err.statusText || 'Something went wrong'
-    })
+      statusText: err.statusText || 'Something went wrong',
+    });
   }
-}
+};
 
 export const Activity = () => {
   const [edit, setEdit] = useState(false);
-  const { job, activity } = useLoaderData();
-  const actionData = useActionData() || [];
-  const navigate = useNavigate();
-  const isEvent = activity.type === 'event';
+  const { job } = useOutletContext();
+  const { activityId } = useParams();
+  const { activities } = job;
 
-  const handleChange = async (e) => {
-    const activityToUpdate = {
-      id: activity.id,
-      done: e.target.checked
-    }
-    const token = authenticate();
-
-    if (!token) {
-      navigate('/')
-    }
-
-    await updateJobActivity(activityToUpdate, token, job.id);
-    navigate(`/jobs/${job.id}/activity/${activity.type}s/${activity.id}`)
-  }
+  const activity = activities.find(activity => activity.id === activityId);
+  const isEvent = activity?.type === 'event';
 
   return (
     <>
@@ -128,15 +82,14 @@ export const Activity = () => {
         />
       </Header>
       <main>
-        <section className="activity-details-group flow">
-          {isEvent ? (
-            <Event activity={activity} edit={edit} onChange={handleChange} />
-          ) : (
-            <Task activity={activity} edit={edit} onChange={handleChange} />
-          )}
-          {edit && actionData.length !== 0
-            ? actionData.map((error) => <Alert key={error.message} message={error.message} />)
-            : null}
+        <section className='activity-details-group flow'>
+          {isEvent
+            ? (
+                <Event activity={activity} edit={edit} setEdit={setEdit} />
+              )
+            : (
+                <Task activity={activity} edit={edit} setEdit={setEdit} />
+              )}
         </section>
       </main>
     </>
