86368696667bc90164267fc6a37a151ced31670c
uncommon-type <uncommon.type@rambler.ru>
2024-07-17 11:12:19 +0300

    [#125] Update approach to state management, user interaction and form handling
    - Add local state management for tracking checkbox state
    
    - Add toggleEditMode handler function to enable or disabling form editing
    
    - Add handleCheckboxChange handler function to update local isChecked
    state, based on user interaction with checkbox
    
    - Add handleCancel function to revert any unsaved changes made during
    edit mode
    
    - Leverage React Router's <fetcher.Form/> and <Form/> to facilitate form
    submission. In read-only mode, use <fetcher.Form/> component for automatic
    submission upon checkbox change. In edit mode, use <Form> component for
    handling form submission
    
    - Include hidden inputs to pass additional data with form submission

--- src/screens/activity/components/Event.jsx
+++ src/screens/activity/components/Event.jsx
@@ -1,38 +1,111 @@
-import { Form } from 'react-router-dom';
+import { useState } from 'react';
+import { Form, useFetcher, useSubmit } from 'react-router-dom';
+
+import { getTime, getDate } from '@helpers/datetime';
 
 import { EditableCheckbox } from '@screens/common/EditableCheckbox';
 import { EditableDateTime } from '@screens/common/EditableDateTime';
 import { EditableDescription } from '@screens/common/EditableDescription';
-import { DeleteButton } from '@screens/common/Buttons/DeleteButton';
-import { SaveButton } from '@screens/common/Buttons/SaveButton';
+import { Button } from '@screens/common/Buttons/Button';
 
-export const Event = ({ activity, edit, onChange }) => {
+export const Event = ({ activity, edit, setEdit }) => {
+  const [isChecked, setIsChecked] = useState(activity.done);
+  const fetcher = useFetcher();
+  const submit = useSubmit();
+
+  const toggleEditMode = (val) => {
+    setEdit(!val);
+  };
+
+  const handleCheckboxChange = (val) => {
+    setIsChecked(val);
+  };
+
+  const handleCancel = () => {
+    toggleEditMode(edit);
+    handleCheckboxChange(activity.done);
+  };
+
   return (
-    <Form method="put" className="flow">
-      <EditableCheckbox
-        edit={edit}
-        label="Type (hiring call, interview, etc)"
-        value={activity.title}
-        name="activity"
-        checked={activity.done}
-        onChange={onChange}
-      />
-      <EditableDateTime edit={edit} value={activity.startDate} name="date" />
-      <EditableDescription
-        edit={edit}
-        label="Description"
-        value={activity.description}
-        name="description"
-      />
-      {edit &&
-        <div className="sidebar">
-          <div />
-          <div className="cluster gap-left">
-            <SaveButton />
-            <DeleteButton />
+    <>
+      {!edit && (
+        <>
+          <fetcher.Form method='put' className='flow'>
+            <input type='hidden' name='type' value='event' />
+            <EditableCheckbox
+              edit={edit}
+              label='Type (hiring call, interview, etc)'
+              value={activity.title}
+              name='activity'
+              checked={isChecked}
+              onChange={(e) => {
+                handleCheckboxChange(e.target.checked);
+                fetcher.submit(e.target.form, { method: 'PUT' });
+              }}
+            />
+          </fetcher.Form>
+          <EditableDateTime edit={edit} value={activity.startDate} name='date' />
+          <EditableDescription
+            edit={edit}
+            label='Description'
+            value={activity.description}
+            name='description'
+          />
+        </>
+      )}
+      {edit && (
+        <Form
+          method='put'
+          className='flow'
+          onSubmit={(e) => {
+            submit(e.target.form, { method: 'PUT' });
+            toggleEditMode(edit);
+          }}
+        >
+          <input type='hidden' name='type' value='event' />
+          <input
+            name='activityTitle'
+            type='hidden'
+            value={activity.title}
+          />
+          <input
+            name='description'
+            type='hidden'
+            value={activity.description}
+          />
+          <input
+            name='time'
+            type='hidden'
+            value={getTime(activity.startDate)}
+          />
+          <input
+            name='date'
+            type='hidden'
+            value={getDate(activity.startDate)}
+          />
+          <EditableCheckbox
+            edit={edit}
+            label='Type (hiring call, interview, etc)'
+            value={activity.title}
+            name='activity'
+            checked={isChecked}
+            onChange={(e) => {
+              handleCheckboxChange(e.target.checked);
+            }}
+          />
+          <EditableDateTime edit={edit} value={activity.startDate} name='date' />
+          <EditableDescription
+            edit={edit}
+            label='Description'
+            value={activity.description}
+            name='description'
+          />
+          <div className='cluster gap-left'>
+            <Button label='Save' aria-label='Save job' variant='primary' type='submit' />
+            <Button label='Cancel' aria-label='Cancel editing' variant='primary' onClick={handleCancel} type='button' />
           </div>
-        </div>
-      }
-    </Form>
+        </Form>
+      )}
+    </>
   );
 };
--- src/screens/activity/components/Task.jsx
+++ src/screens/activity/components/Task.jsx
@@ -1,37 +1,98 @@
-import { Form } from 'react-router-dom';
+import { useState } from 'react';
+import { Form, useFetcher, useSubmit } from 'react-router-dom';
 
 import { EditableCheckbox } from '@screens/common/EditableCheckbox';
 import { EditableDescription } from '@screens/common/EditableDescription';
-import { DeleteButton } from '@screens/common/Buttons/DeleteButton';
-import { SaveButton } from '@screens/common/Buttons/SaveButton';
+import { Button } from '@screens/common/Buttons/Button';
 
-export const Task = ({ activity, edit, onChange }) => {
+export const Task = ({ activity, edit, setEdit }) => {
+  const [isChecked, setIsChecked] = useState(activity.done);
+  const fetcher = useFetcher();
+  const submit = useSubmit();
+
+  const toggleEditMode = (val) => {
+    setEdit(!val);
+  };
+
+  const handleCheckboxChange = (val) => {
+    setIsChecked(val);
+  };
+
+  const handleCancel = () => {
+    toggleEditMode(edit);
+    handleCheckboxChange(activity.done);
+  };
+
   return (
-    <Form method="put" className="flow">
-      <EditableCheckbox
-        edit={edit}
-        label="Short title"
-        value={activity.title}
-        name="activity"
-        checked={activity.done}
-        onChange={onChange}
-      />
-      <EditableDescription
-        edit={edit}
-        label="Description"
-        value={activity.description}
-        name="description"
-      />
-      {edit &&
-        <div className="sidebar">
-          <div />
-          <div className="cluster gap-left">
-            <SaveButton />
-            <DeleteButton />
+    <>
+      {!edit
+      && (
+        <>
+          <fetcher.Form method='put' className='flow'>
+            <input type='hidden' name='type' value='task' />
+            <EditableCheckbox
+              edit={edit}
+              label='Type (hiring call, interview, etc)'
+              value={activity.title}
+              name='activity'
+              checked={isChecked}
+              onChange={(e) => {
+                handleCheckboxChange(e.target.checked);
+                fetcher.submit(e.target.form, { method: 'PUT' });
+              }}
+            />
+          </fetcher.Form>
+          <EditableDescription
+            edit={edit}
+            label='Description'
+            value={activity.description}
+            name='description'
+          />
+        </>
+      )}
+      {edit
+      && (
+        <Form
+          method='put'
+          className='flow'
+          onSubmit={(e) => {
+            submit(e.target.form, { method: 'PUT' });
+            toggleEditMode(edit);
+          }}
+        >
+          <input type='hidden' name='type' value='task' />
+          <input
+            name='activityTitle'
+            type='hidden'
+            value={activity.title}
+          />
+          <input
+            name='description'
+            type='hidden'
+            value={activity.description}
+          />
+          <EditableCheckbox
+            edit={edit}
+            label='Type (hiring call, interview, etc)'
+            value={activity.title}
+            name='activity'
+            checked={isChecked}
+            onChange={(e) => {
+              handleCheckboxChange(e.target.checked);
+            }}
+          />
+          <EditableDescription
+            edit={edit}
+            label='Description'
+            value={activity.description}
+            name='description'
+          />
+          <div className='cluster gap-left'>
+            <Button label='Save' aria-label='Save job' variant='primary' type='submit' />
+            <Button label='Cancel' aria-label='Cancel editing' variant='primary' onClick={handleCancel} type='button' />
           </div>
-        </div>
-      }
-    </Form>
+        </Form>
+      )}
+    </>
   );
 };
-
