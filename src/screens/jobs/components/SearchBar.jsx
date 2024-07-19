107ad038fea3141042c1960f127fef4f0ab681f6
uncommon-type <uncommon.type@rambler.ru>
2024-07-19 10:08:53 +0300

    [#128] Add custom <Button/> component to <Searchbar />

--- src/screens/jobs/components/SearchBar.jsx
+++ src/screens/jobs/components/SearchBar.jsx
@@ -1,11 +1,11 @@
+import { Button } from '@screens/common/Buttons/Button';
+
 export const SearchBar = () => (
   <form className='search-group'>
     <label htmlFor='search'>
       <span className='sr-only'>Search</span>
       <input id='search' type='search' />
     </label>
-    <button type='submit' className='button' data-type='naked'>
-      Search
-    </button>
+    <Button label='Search' aria-label='Search' variant='naked' type='submit' />
   </form>
 );
