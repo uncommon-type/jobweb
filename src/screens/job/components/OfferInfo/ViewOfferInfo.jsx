961240431b5f5a7971c894a48912a2f27a6513d6
uncommon-type <uncommon.type@rambler.ru>
2024-07-22 09:49:02 +0300

    [#129] Transform start date into more readable format for display purposes by using getDate helper function

--- src/screens/job/components/OfferInfo/ViewOfferInfo.jsx
+++ src/screens/job/components/OfferInfo/ViewOfferInfo.jsx
@@ -1,8 +1,10 @@
 import { formatMoney } from '@helpers/form';
+import { getDate } from '@helpers/datetime';
+
 import { ProsAndConsContainer } from './ProsAndCons/ProsAndConsContainer';
 
 export const ViewOfferInfo = ({ job }) => (
-  <form className='flow'>
+  <>
     <div>
       <h4 className='font-special'>Salary per year</h4>
       <p>{job.salary ? formatMoney(job.salary) : 'n/a'}</p>
@@ -13,12 +15,12 @@ export const ViewOfferInfo = ({ job }) => (
     </div>
     <div>
       <h4 className='font-special'>Start date</h4>
-      <p>{job.startDate || 'n/a'}</p>
+      <p>{getDate(job.startDate) || 'n/a'}</p>
     </div>
     <div>
       <h4 className='font-special'>Probation</h4>
       <p>{job.probation || 'n/a'}</p>
     </div>
     <ProsAndConsContainer job={job} />
-  </form>
+  </>
 );
