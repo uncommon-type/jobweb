05ad8e770231e2f25429664b38cc1ff90207ad98
uncommon-type <uncommon.type@rambler.ru>
2024-08-05 15:10:12 +0300

    [#138] Implement dynamic styling for <Input/> component, update conditional rendering of label
    - Add className prop to <Input/> component, enabling customisation of
    component's appearance beyond predefined styles
    
    - Render conditionally label inside a <span> with font-special class,
    based on showLabel prop
    
    - Give 'width-20' className to  <Alert> component

--- src/screens/common/Inputs/Input.jsx
+++ src/screens/common/Inputs/Input.jsx
@@ -2,32 +2,31 @@ import { useId } from 'react';
 
 import { Alert } from '../Alert';
 
-export const Input = ({ label, value, name, id, showLabel, error }) => {
+export const Input = ({ label, value, name, id, showLabel, error, className = '' }) => {
   const generatedId = useId();
   const appliedId = id || generatedId;
 
   return (
     <div className='form-item'>
-      <label htmlFor={`${name}-${appliedId}`} className='font-special'>
+      <label htmlFor={`${name}-${appliedId}`} className={`${className} font-special`}>
         {showLabel
           ? (
-              label
+              <span className='font-special'>
+                {label}
+              </span>
             )
           : (
               <span className='sr-only'>{`Edit ${name} name`}</span>
             )}
-
-        <div>
-          <input
-            type='text'
-            id={`${name}-${appliedId}`}
-            name={name}
-            defaultValue={value}
-            className='width-20'
-          />
-          {error && <Alert>{error}</Alert>}
-        </div>
       </label>
+      <input
+        type='text'
+        id={`${name}-${appliedId}`}
+        name={name}
+        defaultValue={value}
+        className='width-20'
+      />
+      {error && <Alert className='width-20'>{error}</Alert>}
     </div>
   );
 };
