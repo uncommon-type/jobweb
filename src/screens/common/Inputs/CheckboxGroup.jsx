1c7162ee778c01176d76a82b3f355377e5413b37
uncommon-type <uncommon.type@rambler.ru>
2024-08-07 11:11:00 +0300

    [#145] Update handling of type attribute of <CheckboxGroup/> options and how URL for fetcher.submit calls is constructed
    - Allow type value to be determined dynamically based on option's id
    
    - Construct URL for fetcher.submit calls using jobId prop

--- src/screens/common/Inputs/CheckboxGroup.jsx
+++ src/screens/common/Inputs/CheckboxGroup.jsx
@@ -4,45 +4,35 @@ import { TrashIcon } from '@heroicons/react/24/outline';
 
 import { EditableCheckbox } from '../EditableCheckbox';
 
-export const CheckboxGroup = ({ options, edit, jobId }) => {
+export const CheckboxGroup = ({ options, edit, jobId, getActivityTypeById }) => {
   const fetcher = useFetcher();
 
   const handleChange = async (e) => {
-    fetcher.submit({ id: e.target.id, done: e.target.checked, type: e.target.form.type.value }, { method: 'PUT' });
+    const activityId = e.target.id;
+    const activityType = getActivityTypeById(activityId);
+    fetcher.submit({ id: e.target.id, done: e.target.checked, type: activityType }, { method: 'PUT', action: `/jobs/${jobId}/activity` });
   };
 
   const handleOptionRemove = async (e) => {
-    fetcher.submit({ id: e.target.id }, { method: 'DELETE' });
+    fetcher.submit({ id: e.currentTarget.id }, { method: 'DELETE', action: `/jobs/${jobId}/activity` });
   };
 
   return (
     <div className='flow flow-space-xl option-list'>
-      {options.map(({ id, title, done, type }) => {
+      {options.map(({ id, title, done }) => {
         return (
           <div key={id} className='cluster option-list__inner'>
-            <fetcher.Form method='put' className='full-width'>
-              <input
-                name='id'
-                type='hidden'
-                value={id}
-              />
-              <input
-                name='type'
-                type='hidden'
-                value={type}
-              />
-              <EditableCheckbox
-                edit={edit}
-                name={title}
-                checked={done}
-                value={title}
-                onChange={handleChange}
-                id={id}
-                link={`/jobs/${jobId}/activity/events/${id}`}
-              />
-            </fetcher.Form>
-            <button className='cluster option-button' aria-label='Delete item'>
-              <TrashIcon onClick={handleOptionRemove} id={id} />
+            <EditableCheckbox
+              edit={edit}
+              name={title}
+              checked={done}
+              value={title}
+              onChange={handleChange}
+              id={id}
+              link={`/jobs/${jobId}/activity/events/${id}`}
+            />
+            <button className='cluster option-button' aria-label='Delete item' id={id} onClick={handleOptionRemove}>
+              <TrashIcon />
             </button>
           </div>
         );
