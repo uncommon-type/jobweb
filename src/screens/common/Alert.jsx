aaad6cbe6874d57c83038a0b37c4dcd3ed112ad5
uncommon-type <uncommon.type@rambler.ru>
2024-08-23 07:25:12 +0300

    [#150 ] Add className prop to <Alert/> component to allow custom styling

--- src/screens/common/Alert.jsx
+++ src/screens/common/Alert.jsx
@@ -1,7 +1,7 @@
 import { ExclamationTriangleIcon as AlertIcon } from '@heroicons/react/24/outline';
 
-export const Alert = ({ message }) => (
-  <figure className='alert cluster'>
+export const Alert = ({ message, className = '' }) => (
+  <figure className={`alert cluster ${className}`}>
     <AlertIcon />
     <p>{message}</p>
   </figure>
