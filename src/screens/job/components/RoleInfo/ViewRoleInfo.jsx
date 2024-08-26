d7cfb01d7c03f531e771b0cff11dec5f436085b9
uncommon-type <uncommon.type@rambler.ru>
2024-08-26 09:48:39 +0300

    [#160] Include "Stage" section to provide additional information about job application's current stage

--- src/screens/job/components/RoleInfo/ViewRoleInfo.jsx
+++ src/screens/job/components/RoleInfo/ViewRoleInfo.jsx
@@ -3,10 +3,13 @@ import { formatMoney } from '@helpers/form';
 export const ViewRoleInfo = ({ job }) => (
   <div className='flow'>
     <div>
+      <h4 className='font-special'>Stage</h4>
+      <p>{job.status}</p>
+    </div>
+    <div>
       <h4 className='font-special'>Description</h4>
       <p>{job.description || 'n/a'}</p>
     </div>
-
     <div>
       <h4 className='font-special'>Employment type</h4>
       <p>{job.employmentType || 'n/a'}</p>
