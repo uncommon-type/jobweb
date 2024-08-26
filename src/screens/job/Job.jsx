cf0b1ca379aeb9fda7987664ca4c5cc89728779b
uncommon-type <uncommon.type@rambler.ru>
2024-08-26 11:36:35 +0300

    [#161] Add form submission, error handling and data sharing to <Job/> component
    - Add action function to handle form submission and job update
    
    - Within action catch block, implement logic to differentiate between
    client-side and server-side errors so that if there are client-side errors,
    it's possible to show relevant error messages next to form fields
    
    - Add validateErrors function that formats errors
    
    - Pass job data, edit state and from location through context in <Job/>
    component

--- src/screens/job/Job.jsx
+++ src/screens/job/Job.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react';
 import { useLocation, useLoaderData, redirect, Outlet } from 'react-router-dom';
 
 import { authenticate } from '@helpers/token';
-import { getJob } from '@network/jobs';
+import { getJob, updateJob } from '@network/jobs';
+import { validateErrors } from '@screens/addJob/helpers/validate-job';
 
 export const loader = async ({ params }) => {
   const token = authenticate();
@@ -23,13 +24,103 @@ export const loader = async ({ params }) => {
   }
 };
 
+export const action = async ({ request, params }) => {
+  const token = authenticate();
+
+  if (!token) {
+    return redirect('/');
+  }
+
+  const formData = await request.formData();
+  const { jobId } = params;
+
+  const {
+    tabName,
+    description,
+    employmentType,
+    location,
+    salary,
+    status,
+    companyId,
+    companyName,
+    companyDescription,
+    benefits,
+    size,
+    time,
+    date,
+    probation,
+  } = Object.fromEntries(formData.entries(formData));
+
+  const jobToUpdate = { id: jobId, tabName };
+
+  if (description) {
+    jobToUpdate.description = description;
+  }
+
+  if (status) {
+    jobToUpdate.status = status;
+  }
+
+  if (employmentType) {
+    jobToUpdate.employmentType = employmentType;
+  }
+
+  if (location) {
+    jobToUpdate.location = location;
+  }
+
+  if (salary) {
+    jobToUpdate.salary = parseInt(salary);
+  }
+
+  if (tabName === 'company') {
+    jobToUpdate.company = {
+      id: companyId,
+      name: companyName,
+      description: companyDescription,
+
+    };
+
+    if (size) {
+      jobToUpdate.company.size = parseInt(size);
+    }
+  }
+
+  if (benefits) {
+    jobToUpdate.benefits = benefits;
+  }
+
+  if (time && date) {
+    jobToUpdate.startDate = new Date(`${date} ${time}`).toISOString();
+  }
+
+  if (probation) {
+    jobToUpdate.probation = probation;
+  }
+
+  try {
+    return await updateJob(jobId, jobToUpdate, token);//
+  }
+  catch (err) {
+    if (err.status !== 400) {
+      throw new Response('', {
+        status: err.status || 500,
+        statusText: err.statusText || 'Something went wrong',
+      });
+    }
+
+    return validateErrors(err.errors);
+  }
+};
+
 export const Job = () => {
   const [edit, setEdit] = useState(false);
   const job = useLoaderData();
+
   const location = useLocation();
   const from = location.state?.from || '/jobs';
 
   return (
-    <Outlet context={{ job, from, edit, setEdit }} />
+    <Outlet context={{ job, edit, setEdit, from }} />
   );
 };
