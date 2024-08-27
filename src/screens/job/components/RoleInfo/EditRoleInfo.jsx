ccf07f2af80dac7dcb461111db049ad7e5c20b6a
uncommon-type <uncommon.type@rambler.ru>
2024-08-27 09:11:22 +0300

    [#163] Enhance <EditRoleInfo/> component with error handling, save/cancel functionality and job status selection
    - Ensure <EditRoleInfo/> component accepts job, edit, onEdit and errors
    props
    
    - Extract error messages for existing fields, using getErrorMessage helper
    function
    
    - Implement error handling for each input field. Add error props to make
    it possible to show error messages when inputs don't match expectations
    
    - Add <Select/> component to allow users to choose status of their job
    applications
    
    - Use defaultValue prop to set initial selected option for <RadioGroup/>
    components. Derive defaultValue from employment type and location of job
    being edited
    
    - Add save and cancel buttons to allow users to save or cancel their edits

--- src/screens/job/components/RoleInfo/EditRoleInfo.jsx
+++ src/screens/job/components/RoleInfo/EditRoleInfo.jsx
@@ -1,36 +1,77 @@
+import { getErrorMessage } from '@helpers/form';
+
+import { Select } from '@screens/common/Inputs/Select/Select';
 import { TextAreaInput } from '@screens/common/Inputs/TextAreaInput';
 import { RadioGroup } from '@screens/common/Inputs/RadioGroup';
-import { EditableSalaryInput } from '@screens/common/EditableSalaryInput';
+import { EditableSalary } from '@screens/common/EditableSalary';
+import { Button } from '@screens/common/Buttons/Button';
 
-const employmentOptions = ['Full-time', 'Part-time', 'Temporary'];
-const locationOptions = ['Hybrid', 'In Office', 'Remote'];
+const EMPLOYMENT_OPTIONS = ['Full-time', 'Part-time', 'Temporary'];
+const LOCATION_OPTIONS = ['Hybrid', 'In office', 'Remote'];
+const STATUS_OPTIONS = [
+  { value: 'Interested', label: 'Interested' },
+  { value: 'Applied', label: 'Applied' },
+  { value: 'Interviewing', label: 'Interviewing' },
+  { value: 'Offer received', label: 'Offer received' },
+];
 
-export const EditRoleInfo = ({ job, edit }) => (
-  <form className='flow'>
-    <TextAreaInput
-      edit={edit}
-      showLabel={true}
-      label='Description'
-      value={job.description}
-      name='description'
-    />
-    <RadioGroup
-      options={employmentOptions}
-      label='Employment type'
-      name='employmentType'
-      className='cluster'
-    />
-    <RadioGroup
-      options={locationOptions}
-      label='Location'
-      name='location'
-      className='cluster'
-    />
-    <EditableSalaryInput
-      label='Salary per year, in pounds'
-      name='salary'
-      value={job.salary}
-      className='input width-8'
-    />
-  </form>
-);
+export const EditRoleInfo = ({ job, edit, onEdit, errors }) => {
+  const roleDescriptionError = getErrorMessage(errors, 'description');
+  const employmentTypeError = getErrorMessage(errors, 'employmentType');
+  const locationError = getErrorMessage(errors, 'location');
+  const salaryError = getErrorMessage(errors, 'salary');
+
+  const handleCancel = () => {
+    onEdit(!edit);
+  };
+
+  return (
+    <>
+      <input
+        name='tabName'
+        type='hidden'
+        value='role'
+      />
+      <Select
+        id={job.id}
+        name='status'
+        className='width-9'
+        options={STATUS_OPTIONS}
+        defaultValue={job.status}
+      />
+      <TextAreaInput
+        edit={edit}
+        showLabel={true}
+        label='Description'
+        value={job.description}
+        name='description'
+        error={roleDescriptionError}
+      />
+      <RadioGroup
+        options={EMPLOYMENT_OPTIONS}
+        label='Employment type'
+        name='employmentType'
+        error={employmentTypeError}
+        defaultValue={job.employmentType}
+      />
+      <RadioGroup
+        options={LOCATION_OPTIONS}
+        label='Location'
+        name='location'
+        error={locationError}
+        defaultValue={job.location}
+      />
+      <EditableSalary
+        label='Salary per year, in pounds'
+        name='salary'
+        value={job.salary}
+        className='input width-8'
+        error={salaryError}
+      />
+      <div className='cluster'>
+        <Button label='Save' aria-label='Save job' variant='primary' />
+        <Button label='Cancel' aria-label='Cancel editing' variant='primary' onClick={handleCancel} type='button' />
+      </div>
+    </>
+  );
+};
