11df07928d7a0df63080a5bf3a0bf5236c56cc77
uncommon-type <uncommon.type@rambler.ru>
2024-08-28 09:07:39 +0300

    [#164] Update prop handling for job role tab panel in both view and edit modes
    - Pass edit, onEdit and errors props to <EditRoleInfo/> child component
    
    - Provide edit prop to indicate whether it's in edit mode, errors prop
    to display and manage any encountered errors, onEdit={setEdit} prop for
    toggling edit mode
    
    - Pass job details to <ViewRoleInfo/> component via job prop

--- src/screens/job/components/RoleInfo/RoleInfo.jsx
+++ src/screens/job/components/RoleInfo/RoleInfo.jsx
@@ -4,10 +4,10 @@ import { EditRoleInfo } from './EditRoleInfo';
 import { ViewRoleInfo } from './ViewRoleInfo';
 
 export const RoleInfo = () => {
-  const { job, edit } = useOutletContext();
+  const { job, edit, setEdit, errors } = useOutletContext();
 
   if (edit) {
-    return <EditRoleInfo job={job} edit={edit} />;
+    return <EditRoleInfo job={job} edit={edit} onEdit={setEdit} errors={errors} />;
   }
 
   return <ViewRoleInfo job={job} />;
