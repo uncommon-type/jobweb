88c35499e2cb14eb6f766283b85ea94f23070577
uncommon-type <uncommon.type@rambler.ru>
2024-08-31 12:18:47 +0300

    [#167] Enhance <EditOfferInfo/> component with error handling, save/cancel functionality and job status selection
    - Ensure <EditOfferInfo/> component accepts job, edit, onEdit and errors
    props
    
    - Extract error messages for existing fields, using getErrorMessage helper
    function
    
    - Implement error handling for each input field. Add error props to make
    it possible to show error messages when inputs don't match expectations
    
    - Add save and cancel buttons to allow users to save or cancel their edits
    
    - Add handleCancel function for toggling editing state

--- src/screens/job/components/OfferInfo/EditOfferInfo.jsx
+++ src/screens/job/components/OfferInfo/EditOfferInfo.jsx
@@ -1,31 +1,62 @@
-import { EditableSalaryInput } from '@screens/common/EditableSalaryInput';
+import { getErrorMessage } from '@helpers/form';
+
+import { EditableSalary } from '@screens/common/EditableSalary';
 import { TextAreaInput } from '@screens/common/Inputs/TextAreaInput';
 import { DateTimeInput } from '@screens/common/Inputs/DateTimeInput';
 import { ProsAndConsContainer } from './ProsAndCons/ProsAndConsContainer';
+import { Button } from '@screens/common/Buttons/Button';
 
-export const EditOfferInfo = ({ job, edit }) => (
-  <form className='flow'>
-    <EditableSalaryInput
-      label='Salary per year, in pounds'
-      name='salary'
-      value={job.salary}
-      className='input width-8'
-    />
-    <TextAreaInput
-      edit={edit}
-      showLabel={true}
-      label='Benefits'
-      value={job.benefits}
-      name='benefits'
-    />
-    <DateTimeInput date={job.startDate} />
-    <TextAreaInput
-      edit={edit}
-      showLabel={true}
-      label='Probation'
-      value={job.probation}
-      name='probation'
-    />
-    <ProsAndConsContainer job={job} />
-  </form>
-);
+export const EditOfferInfo = ({ job, edit, onEdit, errors }) => {
+  const salaryError = getErrorMessage(errors, 'salary');
+  const benefitsError = getErrorMessage(errors, 'benefits');
+  const probationError = getErrorMessage(errors, 'probation');
+  const dateTimeInputError = getErrorMessage(errors, 'startDate');
+
+  const handleCancel = () => {
+    onEdit(!edit);
+  };
+
+  return (
+    <>
+      <input
+        name='tabName'
+        type='hidden'
+        value='offer'
+      />
+      <EditableSalary
+        label='Salary per year, in pounds'
+        name='salary'
+        value={job.salary}
+        className='input width-8'
+        error={salaryError}
+      />
+      <TextAreaInput
+        edit={edit}
+        showLabel={true}
+        label='Benefits'
+        value={job.benefits}
+        name='benefits'
+        error={benefitsError}
+      />
+      <DateTimeInput
+        date={job.startDate}
+        error={dateTimeInputError}
+      />
+      <TextAreaInput
+        edit={edit}
+        showLabel={true}
+        label='Probation'
+        value={job.probation}
+        name='probation'
+        error={probationError}
+      />
+      <ProsAndConsContainer
+        job={job}
+      />
+      <div className='cluster'>
+        <Button label='Save' aria-label='Save job' variant='primary' />
+        <Button label='Cancel' aria-label='Cancel editing' variant='primary' onClick={handleCancel} type='button' />
+      </div>
+    </>
+  );
+};
