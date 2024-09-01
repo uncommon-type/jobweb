841c0b4e487c06c37375a91e25dd103a60888444
uncommon-type <uncommon.type@rambler.ru>
2024-09-01 10:33:37 +0300

    [#168] Enhance <EditCompanyInfo/> component with error handling, save/cancel functionality and job status selection
    - Ensure <EditCompanyInfo/> component accepts job, edit, onEdit and
    errors props
    
    - Extract error messages for existing fields, using getErrorMessage helper
    function
    
    - Implement error handling for each input field. Add error props to make
    it possible to show error messages when inputs don't match expectations
    
    - Add save and cancel buttons to allow users to save or cancel their edits
    
    - Add handleCancel function for toggling editing state

--- src/screens/job/components/CompanyInfo/EditCompanyInfo.jsx
+++ src/screens/job/components/CompanyInfo/EditCompanyInfo.jsx
@@ -1,27 +1,62 @@
+import { getErrorMessage } from '@helpers/form';
+
 import { TextAreaInput } from '@screens/common/Inputs/TextAreaInput';
 import { NumericInput } from '@screens/common/Inputs/NumericInput';
+import { Button } from '@screens/common/Buttons/Button';
 
-export const EditCompanyInfo = ({ job, edit }) => (
-  <form className='flow'>
-    <TextAreaInput
-      edit={edit}
-      showLabel={true}
-      label='Description'
-      value={job.company.description}
-      name='description'
-    />
-    <TextAreaInput
-      edit={edit}
-      showLabel={true}
-      label='Benefits'
-      value={job.benefits}
-      name='benefits'
-    />
-    <NumericInput
-      label='Size'
-      value={job.company?.size}
-      className='width-5'
-      name='size'
-    />
-  </form>
-);
+export const EditCompanyInfo = ({ job, edit, onEdit, errors }) => {
+  const sizeError = getErrorMessage(errors, 'company/size');
+  const companyDescriptionError = getErrorMessage(errors, 'company/description');
+  const benefitsError = getErrorMessage(errors, 'benefits');
+
+  const handleCancel = () => {
+    onEdit(!edit);
+  };
+
+  return (
+    <>
+      <input
+        name='tabName'
+        type='hidden'
+        value='company'
+      />
+      <input
+        name='companyId'
+        type='hidden'
+        value={job.company.id}
+      />
+      <input
+        name='companyName'
+        type='hidden'
+        value={job.company.name}
+      />
+      <TextAreaInput
+        edit={edit}
+        showLabel={true}
+        label='Description'
+        value={job.company.description}
+        name='companyDescription'
+        error={companyDescriptionError}
+      />
+      <TextAreaInput
+        edit={edit}
+        showLabel={true}
+        label='Benefits'
+        value={job.benefits}
+        name='benefits'
+        error={benefitsError}
+      />
+      <NumericInput
+        label='Size'
+        value={job.company?.size}
+        className='width-5'
+        name='size'
+        error={sizeError}
+      />
+      <div className='cluster'>
+        <Button label='Save' aria-label='Save job' variant='primary' type='submit' />
+        <Button label='Cancel' aria-label='Cancel editing' variant='primary' onClick={handleCancel} type='button' />
+      </div>
+    </>
+  );
+};
