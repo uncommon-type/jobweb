36f2fd1f05fe9dbd0e08bb9eb9290163a40b4302
uncommon-type <uncommon.type@rambler.ru>
2024-09-07 12:52:07 +0300

    [#175] Add cancel button and handleCancel function to allow users to easily exit edit mode without saving changes
    - Add cancel button to allow users to exit edit mode without saving
    changes
    
    - Add handleCancel function to handle cancel button click and revert to
    original state
    
    - Conditionally render save and cancel buttons, based on `edit` state
    
    - Include `fetcher` object in <Outlet/> context for use in child
    components

--- src/screens/job/components/JobItem.jsx
+++ src/screens/job/components/JobItem.jsx
@@ -5,11 +5,12 @@ import { Header } from '@screens/common/Header/Header';
 import { SecondaryNav } from '@screens/common/Header/SecondaryNav';
 import { Card } from '@screens/common/CardGroup/Card/Card';
 import { JobTabs } from './JobTabs';
+import { Button } from '@screens/common/Buttons/Button';
 
 export const JobItem = () => {
   const { from, job, edit, setEdit } = useOutletContext();
-  const fetcher = useFetcher();
   const location = useLocation();
+  const fetcher = useFetcher();
   const errors = fetcher?.data?.length ? fetcher.data : null;
   const isNotActivityPath = location.pathname.split('/').pop() !== 'activity';
 
@@ -21,6 +22,10 @@ export const JobItem = () => {
 
   const handleEdit = () => {
     setEdit(!edit);
+  };
+
+  const handleCancel = () => {
+    setEdit(false);
   };
 
   return (
@@ -39,7 +44,14 @@ export const JobItem = () => {
           {isNotActivityPath
             ? (
                 <fetcher.Form method='put' className='flow'>
-                  <Outlet context={{ job, edit, setEdit, from, errors }} />
+                  <Outlet context={{ job, edit, setEdit, from, errors, fetcher }} />
+                  {edit
+                  && (
+                    <div className='cluster'>
+                      <Button label='Save' aria-label='Save job' variant='primary' name='intent' value='UPDATE' />
+                      <Button label='Cancel' aria-label='Cancel editing' variant='primary' onClick={handleCancel} type='button' />
+                    </div>
+                  )}
                 </fetcher.Form>
               )
             : <Outlet context={{ job, edit, setEdit, from, errors }} />}
