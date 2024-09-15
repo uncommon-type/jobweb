cb2081238fcd8b5bafc5d0b0020683de411e5efc
uncommon-type <uncommon.type@rambler.ru>
2024-09-15 11:50:16 +0300

    [#192] Enhance <TagInput/> component by adding dynamic props, integrating with global context and implementing form management
    - Modify <TagInput/> to accept name and disabled props
    
    - Use useOutletContext() to access fetcher
    
    - Render add button with optional disabled prop
    
    - Use combination of fetcher.state and fetcher.data with useEffect and
     ref to reset input field after submission
    
    - When fetcher.state is 'idle' and fetcher.data exists, tell input to
    reset itself

--- src/screens/job/components/OfferInfo/Tags/TagInput.jsx
+++ src/screens/job/components/OfferInfo/Tags/TagInput.jsx
@@ -1,18 +1,33 @@
+import { useEffect, useRef } from 'react';
+import { useOutletContext } from 'react-router-dom';
+
 import { Button } from '@screens/common/Buttons/Button';
 
-export const TagInput = ({ edit }) => {
+export const TagInput = ({ name, disabled }) => {
+  const { fetcher } = useOutletContext();
+  const inputValue = useRef(null);
+
+  useEffect(() => {
+    if (fetcher.state === 'idle' && fetcher.data) {
+      inputValue.current.value = '';
+    }
+  }, [fetcher.state, fetcher.data]);
+
   return (
     <div className='tag-form width-10'>
-      <label htmlFor='title'>
-        <span className='sr-only'>Add a tag</span>
+      <label htmlFor={name}>
+        <span className='sr-only'>
+          {`Enter a ${name}`}
+        </span>
         <input
           type='text'
-          id='title'
-          name='title'
-          aria-label='Add a tag'
+          id={name}
+          name={name}
+          ref={inputValue}
+          disabled={disabled}
         />
       </label>
-      {!edit && <Button variant='naked' icon='plusIcon' aria-label='Add tag' type='submit' name='intent' value='ADD' />}
+      <Button variant='naked' icon='plusIcon' aria-label={`Add a ${name}`} type='submit' name='intent' value='ADD' disabled={disabled} />
     </div>
   );
 };
