f2b08428184d6188d22270030c562f55dd8b4aaa
uncommon-type <uncommon.type@rambler.ru>
2024-09-15 10:47:44 +0300

    [#189] Update <TagSection/> to display existing tags and disable input after reaching tag count limit
    - Update <TagInput/> to accept tags, title and fieldName props
    
    - Ensure <TagList/> receives tags props
    
    - Implement tag count limit and disable <TagInput/> when exceeded

--- src/screens/job/components/OfferInfo/Tags/TagSection.jsx
+++ src/screens/job/components/OfferInfo/Tags/TagSection.jsx
@@ -1,19 +1,26 @@
 import { TagInput } from './TagInput';
 import { TagList } from './TagList/TagList';
-import { FieldError } from '@screens/common/Error/FieldError';
 
-export const TagSection = ({ jobId, tags, edit, tagError, title }) => (
-  <div>
-    <h4 className='font-special'>{title}</h4>
-    <p> Add up to 5</p>
-    {tagError && <FieldError error={tagError} />}
-    <div className='splitter'>
-      <TagInput edit={edit} jobId={jobId} />
-      {tags?.length > 0
-        ? (
-            <TagList tags={tags} jobId={jobId} />
-          )
-        : null}
+export const TagSection = ({ tags, title, fieldName }) => {
+  const tagCount = tags.filter(tag => tag.type === fieldName).length;
+  const isDisabled = tagCount > 4;
+
+  return (
+    <div>
+      <h4 className='font-special'>{title}</h4>
+      <p> Add up to 5</p>
+
+      <div className='splitter'>
+        <TagInput
+          name={fieldName}
+          disabled={isDisabled}
+        />
+        {tags?.length > 0
+          ? (
+              <TagList tags={tags} />
+            )
+          : null}
+      </div>
     </div>
-  </div>
-);
+  );
+};
