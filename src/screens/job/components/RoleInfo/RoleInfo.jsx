af5c7a2815d6aad8ef9fc4c673fbbe983482901e
uncommon-type <uncommon.type@rambler.ru>
2024-09-17 09:30:56 +0300

    [#198] Simplify <RoleInfo/> component by removing unnecessary props
    - Use useOutletContext() to access edit value
    
    - Conditionally render <EditRoleInfo/> based on edit value

--- src/screens/job/components/RoleInfo/RoleInfo.jsx
+++ src/screens/job/components/RoleInfo/RoleInfo.jsx
@@ -4,11 +4,11 @@ import { EditRoleInfo } from './EditRoleInfo';
 import { ViewRoleInfo } from './ViewRoleInfo';
 
 export const RoleInfo = () => {
-  const { job, edit, setEdit, errors } = useOutletContext();
+  const { edit } = useOutletContext();
 
   if (edit) {
-    return <EditRoleInfo job={job} edit={edit} onEdit={setEdit} errors={errors} />;
+    return <EditRoleInfo />;
   }
 
-  return <ViewRoleInfo job={job} />;
+  return <ViewRoleInfo />;
 };
