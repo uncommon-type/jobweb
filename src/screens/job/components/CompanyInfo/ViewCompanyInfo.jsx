1e43c6d7aff2a9d4ba3f8e31f1fb88ea3d7b804f
uncommon-type <uncommon.type@rambler.ru>
2024-09-17 09:29:21 +0300

    [#196] Simplify <ViewCompanyInfo/> by removing unnecessary props and updating how job properties are accessed
    - Use useOutletContext() to retrieve job data from shared context set up
    higher in component tree
    
    - Destructure job properties, directly use destructured values in component

--- src/screens/job/components/CompanyInfo/ViewCompanyInfo.jsx
+++ src/screens/job/components/CompanyInfo/ViewCompanyInfo.jsx
@@ -1,9 +1,16 @@
+import { useOutletContext } from 'react-router-dom';
+
 import { JobDetail } from '../JobDetail';
 
-export const ViewCompanyInfo = ({ job }) => (
-  <div className='flow'>
-    <JobDetail title='Description' content={job.company.description} />
-    <JobDetail title='Benefits' content={job.benefits} />
-    <JobDetail title='Size' content={job.company.size} />
-  </div>
-);
+export const ViewCompanyInfo = () => {
+  const { job } = useOutletContext();
+  const { company, benefits } = job;
+
+  return (
+    <div className='flow'>
+      <JobDetail title='Description' content={company.description || 'n/a'} />
+      <JobDetail title='Benefits' content={benefits || 'n/a'} />
+      <JobDetail title='Size' content={company.size || 'n/a'} />
+    </div>
+  );
+};
