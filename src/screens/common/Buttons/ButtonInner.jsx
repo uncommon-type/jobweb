cf737e513d29e4eb5a420215c3e910d3550c3aa5
uncommon-type <uncommon.type@rambler.ru>
2024-09-17 12:33:34 +0300

    [#201] Replace React.Fragment with <span/> within <ButtonInner/> component

--- src/screens/common/Buttons/ButtonInner.jsx
+++ src/screens/common/Buttons/ButtonInner.jsx
@@ -3,10 +3,10 @@ import { Icon } from '../Icon/Icon';
 export const ButtonInner = ({ icon = '', label = '' }) =>
   icon
     ? (
-        <>
+        <span className='with-icon'>
           {label}
           <Icon icon={icon} aria-hidden='true' focusable='false' />
-        </>
+        </span>
       )
     : (
         label
