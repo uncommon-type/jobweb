1029be26addb6a57360a9d5ea5f6165cb4963d81
uncommon-type <uncommon.type@rambler.ru>
2024-09-20 17:54:37 +0300

    [#207] Simplify component structure and move state management outside
    - Update component props to include activity, maxLength, edit, onCancel,
    titleError, descriptionError
    
    - Use 'activity-fetcher' key with useFetcher hook for unique fetcher
    identification and management
    
    - Simplify UI rendering logic based on edit prop

--- src/screens/activity/components/Event.jsx
+++ src/screens/activity/components/Event.jsx
@@ -1,35 +1,18 @@
-import { useState, useEffect } from 'react';
-import { useFetcher, useOutletContext } from 'react-router-dom';
-
-import { getErrorMessage } from '@helpers/form';
+import { useState } from 'react';
+import { useFetcher } from 'react-router-dom';
 
 import { EditableCheckbox } from '@screens/common/EditableCheckbox';
 import { EditableDateTime } from '@screens/common/EditableDateTime';
 import { EditableDescription } from '@screens/common/EditableDescription';
 import { Button } from '@screens/common/Buttons/Button';
 
-export const Event = ({ activity, maxLength }) => {
-  const { edit, setEdit } = useOutletContext();
+export const Event = ({ activity, maxLength, edit, onCancel, titleError, descriptionError }) => {
   const [isChecked, setIsChecked] = useState(activity.done);
-  const fetcher = useFetcher();
-  const errors = fetcher?.data?.length ? fetcher.data : null;
-  const titleError = getErrorMessage(errors, 'title');
-  const descriptionError = getErrorMessage(errors, 'description');
-
-  useEffect(() => {
-    if (fetcher.data && !errors) {
-      setEdit(false);
-    }
-  }, [fetcher.data]);
-
-  const handleCancel = () => {
-    setEdit(false);
-  };
+  const fetcher = useFetcher({ key: 'activity-fetcher' });
 
   return (
-    <>
-      {!edit && (
-        <>
+    edit
+      ? (
           <fetcher.Form method='put' className='flow'>
             <input type='hidden' name='type' value='event' />
             <EditableCheckbox
@@ -38,53 +21,53 @@ export const Event = ({ activity, maxLength }) => {
               value={activity.title}
               name='activity'
               checked={isChecked}
-              onChange={
-                (e) => {
-                  setIsChecked(e.target.checked);
-                  fetcher.submit(e.target.form, { method: 'PUT' });
-                }
-              }
+              onChange={(e) => {
+                setIsChecked(e.target.checked);
+              }}
+              error={titleError}
+              className='align-self'
             />
+            <EditableDateTime edit={edit} value={activity.startDate} name='date' />
+            <EditableDescription
+              edit={edit}
+              label='Description'
+              value={activity.description}
+              name='description'
+              maxLength={maxLength}
+              error={descriptionError}
+            />
+            <div className='cluster gap-left'>
+              <Button label='Save' aria-label='Save job' variant='primary' type='submit' />
+              <Button label='Cancel' aria-label='Cancel editing' variant='primary' onClick={onCancel} type='button' />
+            </div>
           </fetcher.Form>
-          <EditableDateTime edit={edit} value={activity.startDate} name='date' />
-          <EditableDescription
-            edit={edit}
-            label='Description'
-            value={activity.description}
-            name='description'
-          />
-        </>
-      )}
-      {edit && (
-        <fetcher.Form method='put' className='flow'>
-          <input type='hidden' name='type' value='event' />
-          <EditableCheckbox
-            edit={edit}
-            label='Type (hiring call, interview, etc)'
-            value={activity.title}
-            name='activity'
-            checked={isChecked}
-            onChange={(e) => {
-              setIsChecked(e.target.checked);
-            }}
-            error={titleError}
-            className='align-self'
-          />
-          <EditableDateTime edit={edit} value={activity.startDate} name='date' />
-          <EditableDescription
-            edit={edit}
-            label='Description'
-            value={activity.description}
-            name='description'
-            maxLength={maxLength}
-            error={descriptionError}
-          />
-          <div className='cluster gap-left'>
-            <Button label='Save' aria-label='Save job' variant='primary' type='submit' />
-            <Button label='Cancel' aria-label='Cancel editing' variant='primary' onClick={handleCancel} type='button' />
-          </div>
-        </fetcher.Form>
-      )}
-    </>
+        )
+      : (
+          <>
+            <fetcher.Form method='put' className='flow'>
+              <input type='hidden' name='type' value='event' />
+              <EditableCheckbox
+                edit={edit}
+                label='Type (hiring call, interview, etc)'
+                value={activity.title}
+                name='activity'
+                checked={isChecked}
+                onChange={
+                  (e) => {
+                    setIsChecked(e.target.checked);
+                    fetcher.submit(e.target.form, { method: 'PUT' });
+                  }
+                }
+              />
+            </fetcher.Form>
+            <EditableDateTime edit={edit} value={activity.startDate} name='date' />
+            <EditableDescription
+              edit={edit}
+              label='Description'
+              value={activity.description}
+              name='description'
+            />
+          </>
+        )
   );
 };
--- src/screens/activity/components/Task.jsx
+++ src/screens/activity/components/Task.jsx
@@ -1,87 +1,68 @@
-import { useState, useEffect } from 'react';
-import { useFetcher, useOutletContext } from 'react-router-dom';
-
-import { getErrorMessage } from '@helpers/form';
+import { useState } from 'react';
+import { useFetcher } from 'react-router-dom';
 
 import { EditableCheckbox } from '@screens/common/EditableCheckbox';
 import { EditableDescription } from '@screens/common/EditableDescription';
 import { Button } from '@screens/common/Buttons/Button';
 
-export const Task = ({ activity, maxLength }) => {
-  const { edit, setEdit } = useOutletContext();
+export const Task = ({ activity, maxLength, edit, onCancel, titleError, descriptionError }) => {
   const [isChecked, setIsChecked] = useState(activity.done);
-  const fetcher = useFetcher();
-  const errors = fetcher?.data?.length ? fetcher.data : null;
-  const titleError = getErrorMessage(errors, 'title');
-  const descriptionError = getErrorMessage(errors, 'description');
-
-  useEffect(() => {
-    if (fetcher.data && !errors) {
-      setEdit(false);
-    }
-  }, [fetcher.data]);
-
-  const handleCancel = () => {
-    setEdit(false);
-  };
+  const fetcher = useFetcher({ key: 'activity-fetcher' });
 
   return (
-    <>
-      {!edit
-      && (
-        <>
+    edit
+      ? (
           <fetcher.Form method='put' className='flow'>
             <input type='hidden' name='type' value='task' />
             <EditableCheckbox
               edit={edit}
-              label='Type (prep for hiring call, etc)'
+              label='Short title'
               value={activity.title}
               name='activity'
               checked={isChecked}
               onChange={(e) => {
                 setIsChecked(e.target.checked);
-                fetcher.submit(e.target.form, { method: 'PUT' });
               }}
+              error={titleError}
+              className='align-self'
             />
+            <EditableDescription
+              edit={edit}
+              label='Description'
+              value={activity.description}
+              name='description'
+              maxLength={maxLength}
+              error={descriptionError}
+            />
+            <div className='cluster gap-left'>
+              <Button label='Save' aria-label='Save job' variant='primary' type='submit' />
+              <Button label='Cancel' aria-label='Cancel editing' variant='primary' onClick={onCancel} type='button' />
+            </div>
           </fetcher.Form>
-          <EditableDescription
-            edit={edit}
-            label='Description'
-            value={activity.description}
-            name='description'
-          />
-        </>
-      )}
-      {edit
-      && (
-        <fetcher.Form method='put' className='flow'>
-          <input type='hidden' name='type' value='task' />
-          <EditableCheckbox
-            edit={edit}
-            label='Short title'
-            value={activity.title}
-            name='activity'
-            checked={isChecked}
-            onChange={(e) => {
-              setIsChecked(e.target.checked);
-            }}
-            error={titleError}
-            className='align-self'
-          />
-          <EditableDescription
-            edit={edit}
-            label='Description'
-            value={activity.description}
-            name='description'
-            maxLength={maxLength}
-            error={descriptionError}
-          />
-          <div className='cluster gap-left'>
-            <Button label='Save' aria-label='Save job' variant='primary' type='submit' />
-            <Button label='Cancel' aria-label='Cancel editing' variant='primary' onClick={handleCancel} type='button' />
-          </div>
-        </fetcher.Form>
-      )}
-    </>
+        )
+      : (
+          <>
+            <fetcher.Form method='put' className='flow'>
+              <input type='hidden' name='type' value='task' />
+              <EditableCheckbox
+                edit={edit}
+                label='Type (prep for hiring call, etc)'
+                value={activity.title}
+                name='activity'
+                checked={isChecked}
+                onChange={(e) => {
+                  setIsChecked(e.target.checked);
+                  fetcher.submit(e.target.form, { method: 'PUT' });
+                }}
+              />
+            </fetcher.Form>
+            <EditableDescription
+              edit={edit}
+              label='Description'
+              value={activity.description}
+              name='description'
+            />
+          </>
+        )
   );
 };
