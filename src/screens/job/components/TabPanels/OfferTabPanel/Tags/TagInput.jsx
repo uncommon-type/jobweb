a2644de956db208e6444b60ca5e5689bfc4ca942
uncommon-type <uncommon.type@rambler.ru>
2024-09-21 09:19:01 +0300

    [#210] Add error handling and dynamic button labelling within <TagInput/> component
    - Update <TagInput/> to accept name, disabled, errorText props
    
    - Render <FieldError/> component if errorText is provided
    
    - Make button label dynamic, incorporating the field name being tagged
    
    - Rename inputValue reference to inputNode for clarity

--- src/screens/job/components/TabPanels/OfferTabPanel/Tags/TagInput.jsx
+++ src/screens/job/components/TabPanels/OfferTabPanel/Tags/TagInput.jsx
@@ -2,19 +2,21 @@ import { useEffect, useRef } from 'react';
 import { useOutletContext } from 'react-router-dom';
 
 import { Button } from '@screens/common/Buttons/Button';
+import { FieldError } from '@screens/common/Error/FieldError';
 
-export const TagInput = ({ name, disabled }) => {
+export const TagInput = ({ name, disabled, errorText }) => {
   const { fetcher } = useOutletContext();
-  const inputValue = useRef(null);
+  const inputNode = useRef(null);
 
   useEffect(() => {
     if (fetcher.state === 'idle' && fetcher.data) {
-      inputValue.current.value = '';
+      inputNode.current.value = '';
     }
   }, [fetcher.state, fetcher.data]);
 
   return (
     <div className='tag-form width-10'>
+      {errorText && <FieldError error={errorText} />}
       <label htmlFor={name}>
         <span className='sr-only'>
           {`Enter a ${name}`}
@@ -23,11 +25,11 @@ export const TagInput = ({ name, disabled }) => {
           type='text'
           id={name}
           name={name}
-          ref={inputValue}
+          ref={inputNode}
           disabled={disabled}
         />
       </label>
-      <Button variant='naked' icon='plusIcon' aria-label={`Add a ${name}`} type='submit' name='intent' value='ADD' disabled={disabled} />
+      <Button variant='naked' icon='plusIcon' aria-label={`Add a ${name}`} type='submit' name='intent' value={`add-${name}`} disabled={disabled} />
     </div>
   );
 };
