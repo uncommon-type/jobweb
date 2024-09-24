06e97a1df3dc2a0e038995dcaba0e74e606e4e9f
uncommon-type <uncommon.type@rambler.ru>
2024-09-24 13:27:07 +0300

    [#213] Implement error filtering and pass filtered errors to <TagSection/>
    - Use useOutletContext() to access job and errors values
    
    - Implement error filtering for pros and cons tags
    
    - Pass filtered errors to <TagSection/> component

--- src/screens/job/components/TabPanels/OfferTabPanel/ViewOfferTabPanel.jsx
+++ src/screens/job/components/TabPanels/OfferTabPanel/ViewOfferTabPanel.jsx
@@ -7,10 +7,12 @@ import { TagSection } from './Tags/TagSection';
 import { JobDetail } from '../JobDetail';
 
 export const ViewOfferTabPanel = () => {
-  const { job } = useOutletContext();
+  const { job, errors } = useOutletContext();
   const { tags, salary, benefits, startDate, probation } = job;
   const cons = tags.filter(tag => tag.type === 'con');
   const pros = tags.filter(tag => tag.type === 'pro');
+  const proErrors = errors?.filter(error => error.name === 'pro');
+  const conErrors = errors?.filter(error => error.name === 'con');
 
   return (
     <>
@@ -18,8 +20,8 @@ export const ViewOfferTabPanel = () => {
       <JobDetail title='Benefits' content={benefits || 'n/a'} />
       <JobDetail title='Start date' content={startDate ? getDate(startDate) : 'n/a'} />
       <JobDetail title='Probation' content={probation || 'n/a'} />
-      <TagSection tags={pros} title='Pros' fieldName='pro' />
-      <TagSection tags={cons} title='Cons' fieldName='con' />
+      <TagSection tags={pros} title='Pros' fieldName='pro' errors={proErrors} />
+      <TagSection tags={cons} title='Cons' fieldName='con' errors={conErrors} />
     </>
   );
 };
