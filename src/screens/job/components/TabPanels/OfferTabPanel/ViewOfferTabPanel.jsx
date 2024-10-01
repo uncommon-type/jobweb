0b404d30b8385c2f941a5dfa0171ebd7ecf8f764
uncommon-type <uncommon.type@rambler.ru>
2024-10-01 09:25:11 +0300

    [#230] Update how errors are handled and managed within <ViewOfferTabPanel/> component
    - Use useFetcher hook for adding tags
    
    - Use 'add-tag-fetcher' key with useFetcher for unique fetcher identification
    and management
    
    - Check if fetcher has data from tag-adding operation. If errors occur,
    store them in errors variable

--- src/screens/job/components/TabPanels/OfferTabPanel/ViewOfferTabPanel.jsx
+++ src/screens/job/components/TabPanels/OfferTabPanel/ViewOfferTabPanel.jsx
@@ -1,4 +1,4 @@
-import { useOutletContext } from 'react-router-dom';
+import { useOutletContext, useFetcher } from 'react-router-dom';
 
 import { formatMoney } from '@helpers/form';
 import { getDate } from '@helpers/datetime';
@@ -7,16 +7,18 @@ import { TagSection } from './Tags/TagSection';
 import { JobDetail } from '../JobDetail';
 
 export const ViewOfferTabPanel = () => {
-  const { job, errors } = useOutletContext();
+  const { job } = useOutletContext();
   const { tags, salary, benefits, startDate, probation } = job;
-  const cons = tags.filter(tag => tag.type === 'con');
-  const pros = tags.filter(tag => tag.type === 'pro');
+  const fetcher = useFetcher({ key: 'add-tag-fetcher' });
+  const errors = fetcher?.data?.length ? fetcher.data : null;
   const proErrors = errors?.filter(error => error.name === 'pro');
   const conErrors = errors?.filter(error => error.name === 'con');
+  const cons = tags.filter(tag => tag.type === 'con');
+  const pros = tags.filter(tag => tag.type === 'pro');
 
   return (
     <>
-      <JobDetail title='Salary per year' content={salary ? formatMoney(salary) : 'n/a'} formatMoney={formatMoney} />
+      <JobDetail title='Salary per year' content={salary ? formatMoney(salary) : 'n/a'} />
       <JobDetail title='Benefits' content={benefits || 'n/a'} />
       <JobDetail title='Start date' content={startDate ? getDate(startDate) : 'n/a'} />
       <JobDetail title='Probation' content={probation || 'n/a'} />
