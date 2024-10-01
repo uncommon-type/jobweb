aca75726582a1943063fd619f5bf0f841b14c71b
uncommon-type <uncommon.type@rambler.ru>
2024-10-01 11:52:48 +0300

    [#240] Implement reusable <Input/> component that can be used to generate different types of input fields with different labels, error messages, optional buttons and help text
    - Support variety of input types
    
    - Render label and error message alongside input field
    
    - Allow for optional button to be displayed next to input field, with
    configurable properties like variant, icon and click handler
    
    - Allow to add a prefix or suffix to input field for additional context
    or visual elements

---
+++ src/screens/common/Inputs/Input/Input.jsx
@@ -0,0 +1,99 @@
+import { useId, forwardRef } from 'react';
+
+import { Labelled } from '../Label/Labelled';
+import { InlineError } from '@screens/common/Error/InlineError';
+import { Button } from '@screens/common/Buttons/Button';
+
+export const Input = forwardRef(
+  function Input({ id, name, label, labelHidden, action = {}, value = '', error, type = 'text', onChange, disabled, readOnly,
+  pattern, minLength, maxLength, rows, message, className, helpText, prefix, suffix, loading = '', autoCapitalize, autoCorrect,
+  spellCheck, autoComplete, ...otherProps }, ref) {
+    const generatedId = useId();
+    const appliedId = id || generatedId;
+
+    const errorMarkup = error && typeof error !== 'boolean' && (
+      <InlineError error={error} />
+    );
+
+    const actionMarkup = (
+      <Button
+        variant={action.variant}
+        icon={action.icon}
+        aria-label={action.name}
+        disabled={disabled}
+        type={action.type}
+        value={name}
+        onClick={e => action.handler(e)}
+      />
+    );
+
+    const inputMarkup = (
+      <input
+        ref={ref}
+        type={type}
+        id={appliedId}
+        name={name}
+        defaultValue={value}
+        className={prefix && suffix && 'input width-8'}
+        onChange={onChange}
+        pattern={pattern}
+        minLength={minLength}
+        maxLength={maxLength}
+        rows={rows}
+        disabled={disabled}
+        readOnly={readOnly}
+        loading={loading}
+        autoCapitalize={autoCapitalize}
+        autoCorrect={autoCorrect}
+        spellCheck={spellCheck}
+        autoComplete={autoComplete}
+        {...otherProps}
+      />
+    );
+
+    const suffixMarkup = suffix && (
+      <div className='input-suffix' aria-hidden='true'>
+        {suffix}
+      </div>
+    );
+
+    const prefixMarkup = prefix && (
+      <div className='input-prefix' aria-hidden='true'>
+        {prefix}
+      </div>
+    );
+
+    const prefixSuffixWrapperClass = prefix ? 'prefixed-suffixed-input cluster' : '';
+
+    const prefixSuffixMarkup = prefix && suffix
+      ? (
+          <div className={prefixSuffixWrapperClass}>
+            { prefixMarkup }
+            { inputMarkup }
+            { suffixMarkup }
+          </div>
+        )
+      : inputMarkup
+    ;
+
+    return (
+      <Labelled
+        id={appliedId}
+        label={label}
+        labelHidden={labelHidden}
+        helpText={helpText}
+        className={className}
+      >
+        {message && message}
+        {error && errorMarkup}
+        {Object.keys(action).length > 0
+          ? (
+              <div className='cluster'>
+                {prefixSuffixMarkup}
+                {actionMarkup}
+              </div>
+            )
+          : prefixSuffixMarkup}
+      </Labelled>
+    );
+  });
