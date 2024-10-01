b96e9b4ce9f0eee579bf8e99ced475e57a2e4600
uncommon-type <uncommon.type@rambler.ru>
2024-10-01 09:39:12 +0300

    [#237] Implement <ActivityChoices/> component that displays  activity list and allows to update their completion status and delete them
    - Use fetcher for data updates
    
    - Use 'activity-choices-fetcher' key to uniquely identify and manage
    fetcher instance
    
    - Add handleOnChange function to respond to checkbox selections or
    deselections. Whenever checkbox option is selected or deselected, retrieve
    selected activity's ID, completion status and type. Send PUT request to
    /jobs/${jobId}/activity/status to update the activity's status
    
    - Add handleOnRemove. When 'Delete' button is clicked, retrieve activity's
    ID and send DELETE request to /jobs/${jobId}/activity/ended to remove
    activity
    
    - Render activity list using <CheckboxList/>, including pre-selected items
    and event handlers for user interactions. Define action object with
    appropriate properties for delete button

---
+++ src/screens/job/components/TabPanels/ActivityTabPanel/ActivityChoices.jsx
@@ -0,0 +1,28 @@
+import { useFetcher } from 'react-router-dom';
+
+import { CheckboxList } from '@screens/common/Inputs/CheckboxList';
+
+const activityChoicesFetcherKey = 'activity-choices-fetcher';
+
+export const ActivityChoices = ({ choices, jobId }) => {
+  const fetcher = useFetcher({ key: 'activity-choices-fetcher' });
+
+  const handleOnChange = async (e) => {
+    fetcher.submit({ id: e.target.id, activityStatus: e.target.checked, type: e.target.dataset.type }, { method: 'PUT', action: `/jobs/${jobId}/activity/status` });
+  };
+
+  const handleOnRemove = async (e) => {
+    fetcher.submit({ id: e.currentTarget.id }, { method: 'DELETE', action: `/jobs/${jobId}/activity/ended` });
+  };
+
+  return (
+    <CheckboxList
+      name='activityStatus'
+      options={choices}
+      selected={choices.filter(activity => activity.done === true).map(({ value }) => value)}
+      onChange={handleOnChange}
+      fetcherkey={activityChoicesFetcherKey}
+      action={{ name: 'Delete', handler: handleOnRemove, icon: 'trashIcon' }}
+    />
+  );
+};
