76de4deb4574f6de2d8c9c865ed58cf5378a4e61
uncommon-type <uncommon.type@rambler.ru>
2024-10-01 12:13:57 +0300

    [#242]  Update component to extract label markup, introduce labelHidden prop and apply custom styling to label
    - Separate label markup into separate variable for improved readability
    
    - Use labelHidden prop to control label visibility
    
    - Apply custom styling to label when it's not hidden

--- src/screens/common/Inputs/Select/Select.jsx
+++ src/screens/common/Inputs/Select/Select.jsx
@@ -1,8 +1,13 @@
-export const Select = ({ id, label = '', name = '', showLabel = false, className = '', options, defaultValue, ...props }) => (
-  <>
-    <label htmlFor={id} className={showLabel ? '' : 'sr-only'}>
+export const Select = ({ id, label = '', labelHidden = false, options, name = '', className = '', defaultValue, ...props }) => {
+  const labelClassName = labelHidden ? 'sr-only' : 'font-special';
+
+  const labelMarkup = (
+    <label htmlFor={id} className={labelClassName}>
       {label}
     </label>
+  );
+
+  const selectMarkup = (
     <select id={id} name={name} {...props} className={className} defaultValue={defaultValue}>
       {options.map(({ label }) => (
         <option key={label} value={label}>
@@ -10,5 +15,12 @@ export const Select = ({ id, label = '', name = '', showLabel = false, className
         </option>
       ))}
     </select>
-  </>
-);
+  );
+
+  return (
+    <div className='form-item'>
+      {labelMarkup}
+      {selectMarkup}
+    </div>
+  );
+};
