be752381a76afb78e5301db9af2fd08e74ee2c3c
uncommon-type <uncommon.type@rambler.ru>
2024-10-01 09:34:17 +0300

    [#235] Rename <LinkToAddActivity/> to <LinkToNewActivity/>

--- src/screens/job/components/TabPanels/ActivityTabPanel/LinkToNewActivity.jsx
+++ src/screens/job/components/TabPanels/ActivityTabPanel/LinkToNewActivity.jsx
@@ -1,6 +1,6 @@
 import { Button } from '@screens/common/Buttons/Button';
 
-export const LinkToAddActivity = ({ jobId }) => (
+export const LinkToNewActivity = ({ jobId }) => (
   <Button
     to={`/jobs/${jobId}/activity/events/new`}
     state={{ from: `/jobs/${jobId}/activity` }}
