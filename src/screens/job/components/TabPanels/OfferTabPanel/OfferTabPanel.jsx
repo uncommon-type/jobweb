924894ec626dcee361b50412452deb0ff9896bc3
uncommon-type <uncommon.type@rambler.ru>
2024-10-01 09:26:59 +0300

    [#231] Add actions for deleting and adding 'pro' and 'con' tags

--- src/screens/job/components/TabPanels/OfferTabPanel/OfferTabPanel.jsx
+++ src/screens/job/components/TabPanels/OfferTabPanel/OfferTabPanel.jsx
@@ -1,7 +1,103 @@
-import { useOutletContext } from 'react-router-dom';
+import { useOutletContext, redirect } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+
+import { authenticate } from '@helpers/token';
+import { postJobTag, deleteJobTag } from '@network/jobs';
+import { validateErrors } from '@screens/addJob/helpers/validate-job';
 
 import { EditOfferTabPanel } from './EditOfferTabPanel';
 import { ViewOfferTabPanel } from './ViewOfferTabPanel';
+
+export const deleteTagAction = async ({ request, params }) => {
+  const token = authenticate();
+
+  if (!token) {
+    return redirect('/');
+  }
+
+  const { jobId } = params;
+
+  const formData = await request.formData();
+  const formValues = Object.fromEntries(formData);
+  const { tagId } = formValues;
+
+  try {
+    return await deleteJobTag(jobId, tagId, token);
+  }
+  catch (err) {
+    console.error('Error caught while attempting to delete a job tag)', err);
+    throw new Response('', {
+      status: err.status,
+      statusText: err.statusText || 'Something went wrong',
+    });
+  }
+};
+
+export const addProTagAction = async ({ request, params }) => {
+  const token = authenticate();
+
+  if (!token) {
+    return redirect('/');
+  }
+
+  const { jobId } = params;
+
+  const formData = await request.formData();
+  const formValues = Object.fromEntries(formData);
+  const { title: tag } = formValues;
+
+  try {
+    if (tag) {
+      return await postJobTag({ id: uuidv4(), title: tag, type: 'pro', tabName: 'offer' }, token, jobId);
+    }
+
+    return [{ name: 'pro', message: 'This field is required' }];
+  }
+
+  catch (err) {
+    console.error('Error caught while attempting to add a pro tag', err);
+    if (err.status !== 400) {
+      throw new Response('', {
+        status: err.status || 500,
+        statusText: err.statusText || 'Something went wrong',
+      });
+    }
+    return validateErrors(err.errors);
+  }
+};
+
+export const addConTagAction = async ({ request, params }) => {
+  const token = authenticate();
+
+  if (!token) {
+    return redirect('/');
+  }
+
+  const { jobId } = params;
+
+  const formData = await request.formData();
+  const formValues = Object.fromEntries(formData);
+  const { title: tag } = formValues;
+
+  try {
+    if (tag) {
+      return await postJobTag({ id: uuidv4(), title: tag, type: 'con', tabName: 'offer' }, token, jobId);
+    }
+
+    return [{ name: 'con', message: 'This field is required' }];
+  }
+
+  catch (err) {
+    console.error('Error caught while attempting to add a con tag', err);
+    if (err.status !== 400) {
+      throw new Response('', {
+        status: err.status || 500,
+        statusText: err.statusText || 'Something went wrong',
+      });
+    }
+    return validateErrors(err.errors);
+  }
+};
 
 export const OfferTabPanel = () => {
   const { edit } = useOutletContext();
