89130358373b7d25bd4073cc17f4c327c69ab8b5
uncommon-type <uncommon.type@rambler.ru>
2024-10-02 10:17:44 +0300

    [#249] Add className prop to <EditableSalary/>

--- src/screens/job/components/TabPanels/RoleTabPanel/EditRoleTabPanel.jsx
+++ src/screens/job/components/TabPanels/RoleTabPanel/EditRoleTabPanel.jsx
@@ -64,6 +64,7 @@ export const EditRoleTabPanel = () => {
         name='salary'
         label='Salary per year, in pounds'
         value={salary}
+        className='input width-8'
         error={salaryError}
       />
     </>
