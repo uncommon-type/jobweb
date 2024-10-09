f2824504761e1985750ad043ca95f47b7d58761d
uncommon-type <uncommon.type@rambler.ru>
2024-10-09 17:11:27 +0300

    [#260] Hide <EditableCheckbox/>'s label unless specified otherwise
    - If <EditableCheckbox/>'s labelHidden prop is not specified, default it
    to true so that component's label is hidden by default in edit mode
    
    - Allow overriding default behaviour by explicitly specifying labelHidden
    prop

--- src/screens/common/EditableCheckbox.jsx
+++ src/screens/common/EditableCheckbox.jsx
@@ -2,32 +2,30 @@ import { Input } from './Inputs/Input/Input';
 import { Checkbox } from './Inputs/Checkbox/Checkbox';
 
 export const EditableCheckbox = ({ id, label, labelClassName = '', labelHidden, textFieldName, textFieldValue, name, checked = false, edit = false,
-  className, error, onChange, ...otherProps }) => {
-  return (
-    <Checkbox
-      id={id}
-      name={name}
-      label={label}
-      labelHidden={labelHidden}
-      labelClassName={labelClassName}
-      checked={checked}
-      edit={edit}
-      className={className}
-      onChange={onChange}
-      {...otherProps}
-    >
-      {edit
-      && (
-        <Input
-          name={textFieldName}
-          label={label}
-          labelHidden={labelHidden}
-          value={textFieldValue}
-          error={error}
-          className='width-20'
-          edit='true'
-        />
-      )}
-    </Checkbox>
-  );
-};
+  className, error, onChange, ...otherProps }) => (
+  <Checkbox
+    id={id}
+    name={name}
+    label={label}
+    labelHidden={labelHidden === undefined ? true : labelHidden}
+    labelClassName={labelClassName}
+    checked={checked}
+    edit={edit}
+    className={className}
+    onChange={onChange}
+    {...otherProps}
+  >
+    {edit
+    && (
+      <Input
+        name={textFieldName}
+        label={label}
+        labelHidden={labelHidden}
+        value={textFieldValue}
+        error={error}
+        className='width-20'
+        edit='true'
+      />
+    )}
+  </Checkbox>
+);
