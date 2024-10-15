42c786666dc3f1845dad75b229f0bca654ffe1b3
uncommon-type <uncommon.type@rambler.ru>
2024-10-15 10:47:01 +0300

    [#264] Implement <Searchbar/>
    - Use loader function to retrieve initial search query from URL parameters
    
    - Use useLoaderData hook to access fetched query
    
    - Submit form automatically when input changes
    
    - Add accessibility attributes like role='search' and aria-label to form element
    
    - Use custom <Input/> component within form

--- src/screens/jobs/components/SearchBar.jsx
+++ src/screens/jobs/components/SearchBar.jsx
@@ -1,11 +1,32 @@
-import { Button } from '@screens/common/Buttons/Button';
+import { useLoaderData, useFetcher } from 'react-router-dom';
 
-export const SearchBar = () => (
-  <form className='search-group'>
-    <label htmlFor='search'>
-      <span className='sr-only'>Search</span>
-      <input id='search' type='search' />
-    </label>
-    <Button label='Search' aria-label='Search' variant='naked' type='submit' />
-  </form>
-);
+import { Input } from '@screens/common/Inputs/Input/Input';
+import { getErrorMessage } from '@helpers/form';
+
+export const searchLoader = async ({ request }) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get('q') || '';
+  return { q };
+};
+
+export const SearchBar = () => {
+  const { q } = useLoaderData();
+  const fetcher = useFetcher({ key: 'search-fetcher' });
+  const errors = fetcher?.data?.length ? fetcher.data : null;
+  const searchError = getErrorMessage(errors, 'q');
+
+  return (
+    <fetcher.Form id='search-form' role='search' action='/jobs/search'>
+      <Input
+        type='search'
+        id='q'
+        name='q'
+        label='Search'
+        labelHidden
+        aria-label='Search'
+        defaultValue={q}
+        onChange={(e) => { fetcher.submit(e.currentTarget.form); }}
+      />
+    </fetcher.Form>
+  );
+};
